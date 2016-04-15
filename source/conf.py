302fe16f0dd528cbb08e3ab25fe27b09d79e4b77
Patrick Holmes <patrick.j.holmes@gmail.com>
2016-04-15 14:06:30 -0700

    Fix sphinx warning referencing file with no link from TOC

--- source/conf.py
+++ source/conf.py
@@ -89,7 +89,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['community/how_to_report_issues.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
