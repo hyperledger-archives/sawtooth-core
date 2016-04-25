a4ef55d21cdac86058fbce86f8ccca5da131d3cd
Shawn T. Amundson <amundson@bitwise.io>
2016-04-25 10:51:53 -0500

    Add sawtooth-arcade to PYTHONPATH
    This is intended to make it easier to use sawtooth-arcade in the
    vagrant environment.

--- guest-files/local-env.sh
+++ guest-files/local-env.sh
@@ -6,6 +6,7 @@ PYTHONPATH=/project/sawtooth-core
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/build/lib.linux-x86_64-2.7
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-mktplace
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-validator
+PYTHONPATH=$PYTHONPATH:/project/sawtooth-arcade
 export PYTHONPATH
 
 export ENABLE_INTEGRATION_TESTS=1
