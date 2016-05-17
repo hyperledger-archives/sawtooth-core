e2f2dc63654bc773f8444f1f15720e4c46566734
James Mitchell <mitchell@bitwise.io>
2016-05-17 16:42:42 -0500

    Change --config, --url, and --peers arguments from nargs to append
    Argparse arguments with nargs='+' or nargs='*' options are
    problematic for disambiguating optional vs. positional
    arguments on the command line. This change moves these arguments
    to a list append model and provides two mechanisms for specifying
    multiple arguments. Either multiple instances of the argument can
    be provided, like '--url http://localhost:8800
    --url http://localhost:8801' or by providing comma-delmited options
    to a single argument, like this:
    '--url http://localhost:8800,http://localhost:8801'. Both approaches
    can be combined, if desired.

--- tests/unit/test_validator_cli.py
+++ tests/unit/test_validator_cli.py
@@ -180,13 +180,16 @@ class TestValidatorCLI(unittest.TestCase):
         os.environ.clear()
 
         cfg = get_configuration(args=["--url",
-                                      "http://testhost:8888",
-                                      "http://testhost:8889"],
+                                      "http://testhost:8888,"
+                                      "http://testhost:8889",
+                                      "--url",
+                                      "http://testhost:8890"],
                                 config_files_required=False)
 
         self.assertIn("LedgerURL", cfg)
         self.assertIn("http://testhost:8888", cfg["LedgerURL"])
         self.assertIn("http://testhost:8889", cfg["LedgerURL"])
+        self.assertIn("http://testhost:8890", cfg["LedgerURL"])
 
 
 if __name__ == '__main__':
