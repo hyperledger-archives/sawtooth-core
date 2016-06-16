41a442ee8815b9ec772a6678eb3b0f2255f475a6
Shawn T. Amundson <amundson@bitwise.io>
2016-06-16 16:24:50 -0500

    Basic framework for transaction family, with TODOs

--- sawtooth_battleship/txn_family.py
+++ sawtooth_battleship/txn_family.py
@@ -88,7 +88,7 @@ class BattleshipTransaction(transaction.Transaction):
         LOGGER.debug("minfo: %s", repr(minfo))
         self._name = minfo['Name'] if 'Name' in minfo else None
         self._action = minfo['Action'] if 'Action' in minfo else None
-        self._space = minfo['Space'] if 'Space' in minfo else None
+        # TODO: handle 'Board', 'Column', 'Row' 
 
     def __str__(self):
         try:
@@ -128,50 +128,73 @@ class BattleshipTransaction(transaction.Transaction):
         
         LOGGER.debug('checking %s', str(self))
 
+        # Name (of the game) is always required
         if self._name is None or self._name == '':
             raise BattleshipException('name not set')
 
+        # Action is always required
         if self._action is None or self._action == '':
             raise BattleshipException('action not set')
+
+        # The remaining validity rules depend upon which action has
+        # been specified.
 
         if self._action == 'CREATE':    
             if self._name in store:
                 raise BattleshipException('game already exists')
+
+            # TODO: Restrict game name letters and numbers.
+            LOGGER.error("in check_valid, CREATE is not fully implemented")
+        elif self._action == 'JOIN':
+            # TODO: Check that the game can be joined (the state is 'NEW')
+
+            # TODO: Check that self._name is in the store (to verify
+            # that the game exists (see FIRE below).
+
+            # TODO: Validate that self._board is a valid board (right size,
+            # right content.
+
+            # TODO: Remove this logging statement when all other TODOs
+            # have been resolved for JOIN
+            LOGGER.error("in check_valid, JOIN is not fully implemented")
         elif self._action == 'FIRE':
             if self._name not in store:
                 raise BattleshipException('no such game')
             
-            
-            if self._space < 1 or self._space > 25:  
-                raise BattleshipException('invalid choice')
-            
+            # TODO: Check that self._column is valid (letter from A-J)
+
+            # TODO: Check that self._row is valid (number from 1-10)
+
             state = store[self._name]['State']
-            
-            # TIE isn't possible during a battleship 
+
             if state in ['P1-WIN', 'P2-WIN']:
-                raise XoException('game complete')
+                raise BattleshipException('game complete')
 
-            if state == 'P1-NEXT' and 'Player1' in store[self._name]:
-                player1 = store[self._name]['Player1']
-                if player1 != self.OriginatorID:
-                    raise XoException('invalid player 1')
+            # TODO: Check that the state is not 'NEW', which would imply
+            # that the players have not yet joined.
 
-            if state == 'P2-NEXT' and 'Player2' in store[self._name]:
-                player1 = store[self._name]['Player2']
-                if player1 != self.OriginatorID:
-                    raise XoException('invalid player 2')
+            player = None
+            if state == 'P1-NEXT':
+                player = store[self._name]['Player1']
+                if player != self.OriginatorID:
+                    raise BattleshipException('invalid player 1')
+            elif state == 'P2-NEXT':
+                player = store[self._name]['Player2']
+                if player != self.OriginatorID:
+                    raise BattleshipException('invalid player 2')
+            else:
+                raise BattleshipException("invalid state: {}".format(state))
 
-            if store[self._name]['Board'][self._space - 1] != '-':
-                raise XoException('space already bombed')
+            # TODO: Check whether the board's column and row have already been
+            # fired upon.  Note there are two boards, so the board used
+            # depends upon which player's turn it is.
 
-        elif self._action == 'ADD_BOARD':
-            raise BattleshipException('ADD_BOARD not implemented')
-        elif self._action == 'REVEAL':
-            raise BattleshipException('REVEAL not implemented')
+            # TODO: Remove this logging statement when all other TODOs
+            # have been resolved for FIRE
+            LOGGER.error("in check_valid, FIRE is not fully implemented")
         else:
-            raise BattleshipException('invalid action')
+            raise BattleshipException('invalid action: {}'.format(self._action))
 
-        
 
     def apply(self, store):
         """Applies all the updates in the transaction to the transaction
@@ -182,14 +205,51 @@ class BattleshipTransaction(transaction.Transaction):
         """
         LOGGER.debug('apply %s', str(self))
 
-        if self._name in store:
+        if self._action == 'CREATE':
+            store[self._name] = { 'State': 'NEW' }
+        elif self._action == 'JOIN':
             game = store[self._name].copy()
+
+            # TODO: if this is the first JOIN, set Board1 and Player1 in
+            # the store.  if this is the second JOIN, set Board2 and Player2
+            # in the store.  Also, initialie TargetBoard1 and TargetBoard2
+            # as empty.
+
+            # TODO: this should only move to 'P1-NEXT' if both boards have
+            # been entered.
+            game["State"] = 'P1-NEXT'
+
+            # TODO: Remove this logging statement when all other TODOs
+            # have been resolved for JOIN
+            LOGGER.error("in apply, JOIN is not fully implemented")
+
+            store[self._name] = game
+        elif self._action == 'FIRE':
+            game = store[self._name].copy()
+
+            # TODO: Reveal.  Update TargetBoard1 or TargetBoard2 as
+            # appropriate, depending on the current player (determined by
+            # State).  Use the LastFireColumn and LastFireRow.
+
+            # TODO: Update LastFireColumn and LastFireRow in the store so
+            # they can be used for last time.  (Set them to self._column and
+            # self._row.)
+
+            # TODO: detect if the game has been won, changing the State
+            # to P1-WIN or P2-WIN as appropriate
+
+            if game['State'] == 'P1-NEXT':
+                game['State'] = 'P2-NEXT'
+            elif game['State'] == 'P2-NEXT':
+                game['State'] = 'P1-NEXT'
+
+            # TODO: Remove this logging statement when all other TODOs
+            # have been resolved for FIRE
+            LOGGER.error("in apply, FIRE is not fully implemented")
+
+            store[self._name] = game
         else:
-            game = {}
-            state = 'BUILD-BOARDS-NEXT'
-        
-        game['State'] = state
-        store[self._name] = game
+            raise BattleshipException("invalid state: {}".format(state))
 
          
     def dump(self):
@@ -202,5 +262,7 @@ class BattleshipTransaction(transaction.Transaction):
 
         result['Name'] = self._name
         result['Action'] = self._action
+        # TODO: handle 'Board', 'Column', 'Row'; only add them to the result
+        # if they are valid arguments for the Action
 
         return result
