f23c23a2a25d2bfb5fb873b0c26034f3d5a7cbd2
Shawn T. Amundson <amundson@bitwise.io>
2016-07-01 17:14:14 -0500

    Add plugin for rethinkdb

---
+++ plugins/install_rethink.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+set -e
+
+if [ ! -f /etc/debian_version ]; then
+    echo "Skipping $0"
+    exit 0
+fi
+
+cd /vagrant
+
+source /etc/lsb-release
+echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" \
+    | tee /etc/apt/sources.list.d/rethinkdb.list
+
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+apt-get update
+apt-get install -y rethinkdb
+
+mkdir -p /opt/rethinkdb/data
+
+cp guest-files/rethinkdb.instance1.conf \
+    /etc/rethinkdb/instances.d/instance1.conf
+
+/etc/init.d/rethinkdb restart
+
