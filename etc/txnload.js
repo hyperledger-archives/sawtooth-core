1a5e7bc7f735377602c58e8786ec155dfb52f01a
Andrea Gunderson <andreax.gunderson@intel.com>
2016-07-01 15:09:13 -0500

    Add support for LogConfig to txnload

--- etc/txnload.js
+++ etc/txnload.js
@@ -1,8 +1,4 @@
 {
-    ## configuration of logging
-    "LogLevel" : "INFO",
-    "LogFile"  : "{log_dir}/load-{host}.log",
-
     ## load specific configuration
     "Interval" : 10,
     "URLs" : [ "http://localhost:8800" ],
