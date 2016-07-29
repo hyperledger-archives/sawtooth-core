b2cdd92662e0d1ea45633b0933e95e0c9f465dcb
Jamie Jason <jamie.jason@intel.com>
2016-07-29 10:23:25 -0700

    Updated logging to include logging to a file and automatic rotating of the logging file based upon file size.

--- sawtooth/simulator_cli.py
+++ sawtooth/simulator_cli.py
@@ -17,11 +17,12 @@ import os
 import sys
 import argparse
 import logging
+import logging.handlers
 import traceback
 import ConfigParser
+import time
 
 from colorlog import ColoredFormatter
-
 from sawtooth.simulator import SawtoothWorkloadSimulator
 
 LOGGER = logging.getLogger(__name__)
@@ -30,9 +31,9 @@ LOGGER = logging.getLogger(__name__)
 def create_console_handler(verbose_level):
     logger = logging.StreamHandler()
     formatter = ColoredFormatter(
-        "%(log_color)s[%(asctime)s %(levelname)-8s%(module)s]%(reset)s "
-        "%(white)s%(message)s",
-        datefmt="%H:%M:%S",
+        fmt='%(log_color)s[%(asctime)s %(levelname)-8s%(module)s]%(reset)s '
+            '%(white)s%(message)s',
+        datefmt='%H:%M:%S',
         reset=True,
         log_colors={
             'DEBUG': 'cyan',
@@ -41,7 +42,6 @@ def create_console_handler(verbose_level):
             'ERROR': 'red',
             'CRITICAL': 'red',
         })
-
     logger.setFormatter(formatter)
 
     if verbose_level == 0:
@@ -54,10 +54,46 @@ def create_console_handler(verbose_level):
     return logger
 
 
-def setup_loggers(verbose_level):
+def create_file_handler(config):
+    # Make sure that the log dir exists before trying to create the logger
+    log_dir = config.get('Simulator', 'log_dir')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Set up a rotating file handler.  We need to convert the log file max
+    # size from MB to bytes (thus the multiply by 1024^2).  Note that if
+    # maxBytes is zero, automatic rotation will not happen (zero is the
+    # default value for 'log_max_size' if it is not set).
+    logger = \
+        logging.handlers.RotatingFileHandler(
+            filename=os.path.join(
+                log_dir,
+                config.get('Simulator', 'log_file')),
+            maxBytes=config.getint('Simulator', 'log_max_size') * 1024 * 1024,
+            backupCount=config.getint('Simulator', 'log_backup_count'))
+    formatter = \
+        logging.Formatter(
+            fmt='[%(asctime)s.%(msecs).03d %(levelname)-8s%(module)s] '
+                '%(message)s',
+            datefmt='%Y-%m-%dT%H:%M:%S')
+
+    formatter.converter = time.gmtime
+
+    logger.setFormatter(formatter)
+    logger.setLevel(logging.INFO)
+
+    return logger
+
+
+def set_up_loggers(config):
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(create_console_handler(verbose_level))
+    logger.addHandler(
+        create_console_handler(
+            verbose_level=config.getint('Simulator', 'verbose')))
+    logger.addHandler(create_file_handler(config))
+
+    return logger
 
 
 def parse_args(args):
@@ -69,27 +105,56 @@ def parse_args(args):
     parser.add_argument('--workload',
                         help='Transaction workload')
     parser.add_argument('--rate',
-                        help='Transaction rate in transactions per minute',
-                        type=int)
+                        type=int,
+                        help='Transaction rate in transactions per minute')
     parser.add_argument('--discover',
+                        type=int,
                         help='How often, in minutes, to refresh validators '
-                             'list',
-                        type=int)
+                             'list')
     parser.add_argument('--config',
                         help='Config file to provide base configuration for '
                              'the simulator and (possibly) the workload '
                              'generator.  Command-line options override '
-                             'corresponding values in the configuration file')
+                             'corresponding values in the configuration file.')
+    parser.add_argument('--log-file',
+                        help='Name of file used for logging')
+    parser.add_argument('--log-dir',
+                        help='Name of directory into which log file will be '
+                             'saved')
+    parser.add_argument('--log-max-size',
+                        type=int,
+                        help='The maximum size, in MB, of the log file '
+                             'before it is rotated.  A value of 0 indicates '
+                             'no maximum size.  If this option is '
+                             'not specified on the command line, is 0, or '
+                             'log_max_size is not specified in the '
+                             'configuration file the log file is not '
+                             'rotated and --log-backup-count is ignored.')
+    parser.add_argument('--log-backup-count',
+                        type=int,
+                        help='The number of backup log files to retain.  If '
+                             'the --log-max-size option is not provided on '
+                             'the command line with a non-zero value and '
+                             'log_max_size is not set to a non-zero value '
+                             'in the configuration file, this option is '
+                             'ignored.')
     parser.add_argument('-v', '--verbose',
                         action='count',
-                        help='Enable more verbose output')
+                        help='Enable more verbose output to the console (can '
+                             'be specified multiple times to generate more '
+                             'verbose logging).  Note that file logging will '
+                             'always use INFO verbosity level.')
 
     opts = parser.parse_args(args)
 
     if opts.rate is not None and opts.rate <= 0:
-        parser.error("Transaction rate must be greater than zero")
+        parser.error('Transaction rate must be greater than zero')
     if opts.discover is not None and opts.discover <= 0:
-        parser.error("Validator discovery period must be greater than 0")
+        parser.error('Validator discovery period must be greater than 0')
+    if opts.log_max_size is not None and opts.log_max_size < 0:
+        parser.error('Maximum log size must be greater than or equal to 0')
+    if opts.log_backup_count is not None and opts.log_backup_count <= 0:
+        parser.error('Backup count must be greater than 0')
 
     return opts
 
@@ -116,6 +181,13 @@ def load_configuration(opts):
                'txnintegration.integer_key_workload.IntegerKeyWorkload')
     config.set('Simulator', 'rate', '12')
     config.set('Simulator', 'discover', '15')
+    config.set('Simulator', 'log_file', 'simulator.log')
+    config.set(
+        'Simulator',
+        'log_dir',
+        os.path.join(os.path.expanduser('~'), '.sawtooth/logs'))
+    config.set('Simulator', 'log_max_size', '0')
+    config.set('Simulator', 'log_backup_count', '10')
     config.set('Simulator', 'verbose', '1')
 
     # If there was a configuration file command-line option, then we
@@ -124,7 +196,7 @@ def load_configuration(opts):
         if os.path.exists(opts.config):
             config.read(opts.config)
         else:
-            raise Exception("Config file does not exist: {}".format(
+            raise Exception('Config file does not exist: {}'.format(
                 opts.config))
 
     # Otherwise, we are going to look in the default area
@@ -134,18 +206,36 @@ def load_configuration(opts):
     else:
         config_file = \
             os.path.join(
-                os.path.expanduser("~"),
-                ".sawtooth",
-                "simulator.cfg")
+                os.path.expanduser('~'),
+                '.sawtooth',
+                'simulator.cfg')
         if os.path.exists(config_file):
             config.read(config_file)
         else:
             if not os.path.exists(os.path.dirname(config_file)):
                 os.makedirs(os.path.dirname(config_file))
 
-            with open("{}.new".format(config_file), "w") as fd:
+            with open('{}.new'.format(config_file), 'w') as fd:
                 config.write(fd)
-            os.rename("{}.new".format(config_file), config_file)
+            os.rename('{}.new'.format(config_file), config_file)
+
+    if config.getint('Simulator', 'rate') <= 0:
+        raise ConfigParser.ParsingError(
+            'Transaction rate in config file must be greater than zero')
+    if config.getint('Simulator', 'discover') <= 0:
+        raise ConfigParser.ParsingError(
+            'Discovery rate in config file must be greater than zero')
+    if config.getint('Simulator', 'log_max_size') < 0:
+        raise ConfigParser.ParsingError(
+            'Maximum log file size in config file must be greater than or '
+            'equal to zero')
+    if config.getint('Simulator', 'log_backup_count') <= 0:
+        raise ConfigParser.ParsingError(
+            'Log file backup count in config file must be greater than zero')
+    if config.getint('Simulator', 'verbose') < 0:
+        raise ConfigParser.ParsingError(
+            'Log file verbosity in config file must be greater than or '
+            'equal to zero')
 
     # Now override any configuration values with corresponding command-line
     # options that exist.
@@ -159,6 +249,14 @@ def load_configuration(opts):
         config.set('Simulator', 'discover', str(opts.discover))
     if opts.verbose is not None:
         config.set('Simulator', 'verbose', str(opts.verbose))
+    if opts.log_file is not None:
+        config.set('Simulator', 'log_file', opts.log_file)
+    if opts.log_dir is not None:
+        config.set('Simulator', 'log_dir', opts.log_dir)
+    if opts.log_max_size is not None:
+        config.set('Simulator', 'log_max_size', str(opts.log_max_size))
+    if opts.log_backup_count is not None:
+        config.set('Simulator', 'log_backup_count', str(opts.log_backup_count))
 
     return config
 
@@ -167,8 +265,7 @@ def main(name=os.path.basename(sys.argv[0]), args=sys.argv[1:]):
 
     opts = parse_args(args)
     config = load_configuration(opts)
-
-    setup_loggers(verbose_level=config.getint('Simulator', 'verbose'))
+    set_up_loggers(config)
 
     LOGGER.info('Simulator configuration:')
     for section in config.sections():
