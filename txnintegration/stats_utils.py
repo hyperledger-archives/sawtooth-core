b3ee1495d53c9283bd154e710cb5268f03810565
Tom Barnes <thomas.j.barnes@intel.com>
2016-08-11 12:44:31 -0700

    add per-validator csv file generation
    see command line help for using per-validator csv file generation
    
    moved summary csv stats logic to SummaryStatsCsvManager()
    placed per-validator csv stats logic in ValidatorStatsCsvManager()
    created DictWalker() to handle iterating through stats dictionary
    resulting from reading http response from validator.  Includes some basic
    error checking that might be handly for integration testing
    
    modified stats client to use validator name from endpoint registry

--- txnintegration/stats_utils.py
+++ txnintegration/stats_utils.py
@@ -37,21 +37,175 @@ class CsvManager(object):
     def csv_append(self, datalist):
         self.csvdata.extend(datalist)
 
-    def csv_write_header(self, headerlist=None):
+    def csv_write_header(self, headerlist=None, add_time=True):
         if headerlist is not None:
             self.csvdata.extend(headerlist)
-        self.csvdata.insert(0, "time")
+        if add_time:
+            self.csvdata.insert(0, "time")
         self._csv_write()
 
-    def csv_write_data(self, datalist=None):
+    def csv_write_data(self, datalist=None, add_time=True):
         if datalist is not None:
             self.csvdata.extend(datalist)
-        self.csvdata.insert(0, time.time())
+        if add_time:
+            self.csvdata.insert(0, time.time())
         self._csv_write()
 
     def _csv_write(self):
         self.writer.writerow(self.csvdata)
         self.csvdata = []
+
+
+class SummaryStatsCsvManager(object):
+    def __init__(self, system_stats, platform_stats):
+        self.csv_enabled = False
+        self.csv_mgr = CsvManager()
+
+        self.ss = system_stats
+        self.ps = platform_stats
+
+    def initialize(self):
+        self.csv_enabled = True
+
+        filename = "summary_stats_" + str(int(time.time())) + ".csv"
+        self.csv_mgr.open_csv_file(filename)
+
+        header = self.ss.get_names()
+        self.csv_mgr.csv_append(header)
+        header = self.ps.get_names()
+        self.csv_mgr.csv_write_header(header)
+
+    def write_stats(self):
+        if self.csv_enabled:
+            data = self.ss.get_data()
+            self.csv_mgr.csv_append(data)
+            data = self.ps.get_data()
+            self.csv_mgr.csv_write_data(data)
+
+    def stop(self):
+        if self.csv_enabled:
+            self.csv_mgr.close_csv_file()
+
+
+class ValidatorStatsCsvManager(object):
+    def __init__(self, client_list):
+        self.csv_enabled = False
+        self.csv_mgr = CsvManager()
+
+        self.get_header = False
+        self.csv_stats = CsvManager()
+        self.clients = client_list
+        self.stat_names = []
+
+        self.dw = DictWalker()
+
+    def initialize(self):
+        self.csv_enabled = True
+
+        filename = "validator_stats_" + str(int(time.time())) + ".csv"
+        self.csv_mgr.open_csv_file(filename)
+
+        # defer writing header until first instance of data dictionary
+        # is available
+        self.get_header = True
+
+    def write_stats(self):
+        current_time = time.time()
+        if self.csv_enabled:
+            for client in self.clients:
+                if client.responding:
+                    self.dw.walk(client.vsm.val_stats)
+                    if self.get_header:
+                        names = self.dw.get_names()
+                        names.insert(0, "validator_name")
+                        names.insert(0, "time")
+                        self.csv_mgr.csv_write_header(names, add_time=False)
+                        self.get_header = False
+                    data = self.dw.get_data()
+                    data.insert(0, client.name)
+                    data.insert(0, current_time)
+                    self.csv_mgr.csv_write_data(data, add_time=False)
+
+    def stop(self):
+        if self.csv_enabled:
+            self.csv_mgr.close_csv_file()
+
+
+class DictWalker(object):
+    def __init__(self):
+        self.name_list = []
+        self.unique_name_list = []
+        self.unique_name_with_comma_list = []
+
+        # generates a new set of key/value pairs
+        # each time walk() is called
+        self.data = dict()
+        # contains a set of keys ordered by entry order
+        # that persists across calls to walk() - this lets you discover
+        # (and remember) new keys as the appear in the data stream
+        self.names = collections.OrderedDict()
+
+        self.value_type_error_count = 0
+        self.name_has_comma_error_count = 0
+
+    def walk(self, data_dict):
+        self.data = dict()
+        return self._traverse_dict(data_dict)
+
+    def _traverse_dict(self, data_dict):
+        for key, value in data_dict.iteritems():
+            self.name_list.append(key)
+            if isinstance(value, dict):
+                self._traverse_dict(value)
+            else:
+                if isinstance(value, (list, tuple)):
+                    self.value_type_error_count += 1
+                    value = "value_type_error"
+                if isinstance(value, str):
+                    if value.find(",") is not -1:
+                        self.value_has_comma_error += 1
+                        value = "value_comma_error"
+
+                unique_name = self._unique_name(self.name_list)
+                if unique_name.find(",") is not -1:
+                    self.name_has_comma_error_count += 1
+                    self.unique_name_with_comma_list.append(unique_name)
+                else:
+                    self.unique_name_list.append(unique_name)
+                    self.data[unique_name] = value
+                    self.names[unique_name] = None
+
+                self.name_list.pop()
+        if len(self.name_list) > 0:
+            self.name_list.pop()
+
+    def get_data(self):
+        # retrieve data using names so it is always reported in the same order
+        # this is important when using get_data() and get_names()
+        # to write header and data to csv file to ensure headers and data
+        # are written in the same order even if the source dictionary changes
+        # size (which it will - at minimum validators do not all process
+        # the same messages
+        data_list = []
+        for name, value in self.names.items():
+            value = self.data.get(name, "no_val")
+            data_list.append(value)
+        return data_list
+
+    def get_names(self):
+        name_list = []
+        for name in self.names:
+            name_list.append(name)
+        return name_list
+
+    def _unique_name(self, name_list):
+        s = ""
+        for name in name_list:
+            if len(s) == 0:
+                s = name
+            else:
+                s = "{}-{}".format(s, name)
+        return s
 
 
 class TransactionRate(object):
