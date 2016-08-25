041df2cf29349fa84531355f6363115990673c03
Shawn T. Amundson <amundson@bitwise.io>
2016-08-25 17:41:50 -0500

    Fix line-too-long lint error in setup.py
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- validator/setup.py
+++ validator/setup.py
@@ -113,8 +113,8 @@ setup(
     author='Mic Bowman, Intel Labs',
     url='http://www.intel.com',
     packages=find_packages(),
-    install_requires=['sawtooth-core', 'colorlog', 'twisted', 'PyYAML', 'psutil',
-                      'numpy', 'networkx', 'daemonize'],
+    install_requires=['sawtooth-core', 'colorlog', 'twisted', 'PyYAML',
+                      'psutil', 'numpy', 'networkx', 'daemonize'],
     data_files=data_files,
     entry_points={
         'console_scripts': [
