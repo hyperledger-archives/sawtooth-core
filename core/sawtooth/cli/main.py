e7941e19be2cc737ad55960cd56022ae84cd08e6
Shawn T. Amundson <amundson@bitwise.io>
2016-09-05 10:27:38 -0500

    Move sawtooth block to a module

--- core/sawtooth/cli/main.py
+++ core/sawtooth/cli/main.py
@@ -35,6 +35,8 @@ from sawtooth.exceptions import ClientException
 from sawtooth.exceptions import InvalidTransactionError
 from sawtooth.exceptions import MessageException
 
+from sawtooth.cli.block import add_block_parser
+from sawtooth.cli.block import do_block
 from sawtooth.cli.exceptions import CliException
 from sawtooth.cli.keygen import add_keygen_parser
 from sawtooth.cli.keygen import do_keygen
@@ -138,52 +140,6 @@ examples:
         action='store_true',
         default=False,
         help='wait for this commit before exiting')
-
-
-def add_block_parser(subparsers, parent_parser):
-    parser = subparsers.add_parser('block')
-
-    grand_parsers = parser.add_subparsers(title='grandchildcommands',
-                                          dest='subcommand')
-
-    epilog = '''
-    details:
-      list the committed block IDs from the newest to the oldest.
-    '''
-
-    list_parser = grand_parsers.add_parser('list', epilog=epilog)
-    list_parser.add_argument(
-        '--url',
-        type=str,
-        help='the URL to the validator')
-    list_parser.add_argument(
-        '--blockcount',
-        type=int,
-        default=10,
-        help='list the maximum number of committed block IDs. Default: 10')
-    list_parser.add_argument(
-        '-a', '--all',
-        action='store_true',
-        help='list all of committed block IDs')
-
-    epilog = '''
-    details:
-      show the contents of block or the value associated with key
-      within the block.
-    '''
-    show_parser = grand_parsers.add_parser('show', epilog=epilog)
-    show_parser.add_argument(
-        'blockID',
-        type=str,
-        help='the id of the block')
-    show_parser.add_argument(
-        '-k', '--key',
-        type=str,
-        help='the key within the block')
-    show_parser.add_argument(
-        '--url',
-        type=str,
-        help='the URL to the validator')
 
 
 def add_transaction_parser(subparsers, parent_parser):
@@ -379,36 +335,6 @@ def _get_webclient(args):
         url = 'http://localhost:8800'
 
     return LedgerWebClient(url)
-
-
-def do_block(args):
-    subcommands = ['list', 'show']
-    if args.subcommand not in subcommands:
-        print 'Unknown sub-command, expecting one of {0}'.format(
-            subcommands)
-        return
-
-    web_client = _get_webclient(args)
-
-    try:
-        if args.subcommand == 'list':
-            if args.all:
-                blockids = web_client.get_block_list()
-            else:
-                blockids = web_client.get_block_list(args.blockcount)
-            for block_id in blockids:
-                print block_id
-            return
-        elif args.subcommand == 'show':
-            if args.key is not None:
-                block_info = web_client.get_block(args.blockID, args.key)
-            else:
-                block_info = web_client.get_block(args.blockID)
-            print pretty_print_dict(block_info)
-            return
-
-    except MessageException as e:
-        raise CliException(e)
 
 
 def do_transaction(args):
