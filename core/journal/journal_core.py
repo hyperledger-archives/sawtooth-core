0b9af98787231e53cadcc71a3c14ec67a526d03d
Shawn T. Amundson <amundson@bitwise.io>
2016-09-05 23:13:51 -0500

    Remove object cache from ThreadSafeShelf
    This caused some issues with mkplace and requires more investigation,
    so reverting for now.

--- core/journal/journal_core.py
+++ core/journal/journal_core.py
@@ -1262,28 +1262,18 @@ class Journal(gossip_core.Gossip):
 class ThreadSafeShelf(object):
     def __init__(self, filename, flag, max_cache_size):
         self._lock = RLock()
-        self._data = OrderedDict()
         self._shelf = Shelf(anydbm.open(filename, flag))
-        self._max_cache_size = max_cache_size
 
     def __setitem__(self, key, value):
         with self._lock:
-            self._data[key] = value
             self._shelf[key] = value
-
-            if len(self._data) > self._max_cache_size:
-                self._data.popitem(last=False)
 
     def __getitem__(self, key):
         with self._lock:
-            if key in self._data:
-                return self._data[key]
             return self._shelf[key]
 
     def __delitem__(self, key):
         with self._lock:
-            if key in self._data:
-                del self._data[key]
             del self._shelf[key]
 
     def __len__(self):
@@ -1296,8 +1286,6 @@ class ThreadSafeShelf(object):
 
     def get(self, key, default=None):
         with self._lock:
-            if key in self._data:
-                return self._data[key]
             return self._shelf.get(key, default)
 
     def sync(self):
