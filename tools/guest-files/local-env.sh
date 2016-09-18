cd379956900a7c10c71738a33b3f05a4f459c62c
Shawn T. Amundson <amundson@bitwise.io>
2016-09-18 10:22:10 -0500

    Remove core build directory from sys.path/PYTHONPATH

--- tools/guest-files/local-env.sh
+++ tools/guest-files/local-env.sh
@@ -3,7 +3,6 @@
 export CURRENCYHOME=/project/sawtooth-core/validator/
 
 PYTHONPATH=/project/sawtooth-core/core
-PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/core/build/lib.linux-x86_64-2.7
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/validator
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/validator/build/lib.linux-x86_64-2.7
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/extensions/mktplace
