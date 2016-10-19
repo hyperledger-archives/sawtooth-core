54abf9fcccb5f57ac1d4e59a2bd7c8ca50d0404f
Shawn T. Amundson <amundson@bitwise.io>
2016-10-19 13:16:43 -0500

    Convert install_docker plugin to use package groups
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- tools/plugins/install_docker.sh
+++ tools/plugins/install_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-. /vagrant/conf.sh
+. /vagrant/func.sh
 
 set -e
 
@@ -9,7 +9,6 @@ if [ ! -f /etc/debian_version ]; then
     exit 0
 fi
 
-apt-get install -y apt-transport-https ca-certificates
 VER=$(lsb_release -sr)
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -20,6 +19,7 @@ else
     echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 fi
 apt-get update
-apt-get install -y --show-progress linux-image-extra-$(uname -r) apparmor docker-engine
+
+package_group_install docker
 
 usermod -aG docker $VAGRANT_USER
