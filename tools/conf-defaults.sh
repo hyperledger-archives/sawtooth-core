b45a779dfc6c9a325f95f4a80febb68a85923d91
Shawn T. Amundson <amundson@bitwise.io>
2016-10-19 10:21:55 -0500

    Remove bootstrap.d install of sawtooth
    These are infrequently used and if we want to maintain them,
    we should recreate plugins which do this functionality instead
    of bootstrap code.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- tools/conf-defaults.sh
+++ tools/conf-defaults.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-[[ -z "$INSTALL_TYPE" ]] && export INSTALL_TYPE=none
 [[ -z "$START_TXNVALIDATOR" ]] && export START_TXNVALIDATOR=no
 [[ -z "$SETUP_SAWTOOTH_ENVIRONMENT" ]] && export SETUP_SAWTOOTH_ENVIRONMENT=yes
 [[ -z "$SETUP_SAWTOOTH_PATH" ]] && export SETUP_SAWTOOTH_PATH=no
