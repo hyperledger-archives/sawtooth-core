09a486e7e1fe8947ae5c16e3b8d07a4d31f6ee19
Shawn T. Amundson <amundson@bitwise.io>
2016-10-19 09:51:06 -0500

    Update latex plugin to use package groups
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- tools/plugins/install_latex.sh
+++ tools/plugins/install_latex.sh
@@ -1,14 +1,7 @@
 #!/bin/bash -x
 
+. /vagrant/func.sh
+
 set -e
 
-if [ -f /etc/debian_version ]; then
-    apt-get install -y -q \
-        texlive-latex-base \
-        texlive-latex-extra \
-        texlive-latex-recommended \
-        texlive-fonts-recommended
-else
-    echo "Skipping Latex installation on this platform."
-fi
-
+package_group_install latex
