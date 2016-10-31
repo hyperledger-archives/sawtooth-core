1bafa988c8ec9238758bdd1ec48199225147e8e9
Andrea Gunderson <andreax.gunderson@intel.com>
2016-10-31 10:18:17 -0500

    Move DevModeTestSuite to tests/sawtooth_suites and add License
    Signed-off-by: Andrea Gunderson <andreax.gunderson@intel.com>

--- validator/tests/sawtooth_suites/dev_mode.py
+++ validator/tests/sawtooth_suites/dev_mode.py
@@ -1,8 +1,22 @@
+# Copyright 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
 import unittest
 import time
 import os
-from test_smoke import TestSmoke
-from test_local_validation import TestLocalValidationErrors
+from integration.test_smoke import TestSmoke
+from integration.test_local_validation import TestLocalValidationErrors
 from txnintegration.simcontroller import get_default_sim_controller
 from txnintegration.validator_network_manager import ValidatorNetworkManager, \
     defaultValidatorConfig
