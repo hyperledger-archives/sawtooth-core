31d0d5cb95158acc2e6474b3a8b1697856108467
ToddX Ojala <toddx.ojala@intel.com>
2016-11-01 15:41:29 -0500

    Fix file used by tutorial after --genesis flag removal
    Minor change to a new bash script that is run as part of the
    tutorial due to the --genesis and --restore flag removal work.
    
    The bash script invokes the new poet-genesis utility in sawtooth CLI.
    This command needs to specify the integer key transaction family.
    It was originally left out, because I thought that this was part
    of the default config. Testing showed this not to be true.

--- docs/source/tutorial/genesis.sh
+++ docs/source/tutorial/genesis.sh
@@ -5,5 +5,5 @@ mkdir /home/vagrant/sawtooth/keys
 mkdir /home/vagrant/sawtooth/data
 mkdir /home/vagrant/sawtooth/logs
 ./bin/sawtooth keygen base000 --key-dir /home/vagrant/sawtooth/keys
-./bin/sawtooth admin poet0-genesis --keyfile /home/vagrant/sawtooth/keys/base000.wif -F mktplace.transactions.market_place
+./bin/sawtooth admin poet0-genesis --keyfile /home/vagrant/sawtooth/keys/base000.wif -F mktplace.transactions.market_place -F ledger.transaction.integer_key
 echo {\"InitialConnectivity\": 0} > /home/vagrant/sawtooth/v0.json
