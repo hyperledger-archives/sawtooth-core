9eaa1a9274dc0702c813309f27893aabe3ca6408
BoydX Johnson <boydx.johnson@intel.com>
2016-11-14 10:31:37 -0600

    Implement java tp client code
    The MessageListener handles all incoming messages, either updating the Futures
    if the message is a response to a sent message, or putting the message on the
    receiveQueue. To get that message any client using Stream would call Stream().receive();
    The State class handles interactions with the validator ContextManager using Stream.
    
    Signed-off-by: BoydX Johnson <boydx.johnson@intel.com>

---
+++ sdk/java/src/main/java/sawtooth/sdk/client/MessageListener.java
@@ -0,0 +1,57 @@
+/* Copyright 2016 Intel Corporation
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+------------------------------------------------------------------------------*/
+
+package sawtooth.sdk.client;
+
+import com.google.common.util.concurrent.SettableFuture;
+import com.google.protobuf.ByteString;
+
+import io.grpc.ClientCall;
+import sawtooth.sdk.protobuf.Message;
+import sawtooth.sdk.protobuf.MessageList;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+class MessageListener extends ClientCall.Listener<MessageList> {
+
+  private ConcurrentHashMap<String, SettableFuture<ByteString>> futureHashMap;
+  private LinkedBlockingQueue<Message> receiveQueue;
+
+  MessageListener(ConcurrentHashMap<String, SettableFuture<ByteString>> futures,
+                  LinkedBlockingQueue<Message> receiveQueue) {
+    super();
+    this.futureHashMap = futures;
+    this.receiveQueue = receiveQueue;
+  }
+
+
+  @Override
+  public void onMessage(MessageList messageList) {
+    for (Message message : messageList.getMessagesList()) {
+      String correlationId = message.getCorrelationId();
+
+      if (this.futureHashMap.containsKey(correlationId)) {
+        SettableFuture<ByteString> future = this.futureHashMap.get(correlationId);
+        future.set(message.getContent());
+        this.futureHashMap.put(correlationId, future);
+      } else {
+
+        this.receiveQueue.add(message);
+      }
+    }
+  }
+
+}
