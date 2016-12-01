23c0e58ffc52f508cb9ed5ed1262b3ddf4858095
nickdrozd <drozd@bitwise.io>
2016-12-01 10:08:33 -0600

    Consolidate tutorial setup, update genesis script, update tutorial
    Signed-off-by: nickdrozd <drozd@bitwise.io>

--- docs/source/tutorial/genesis.sh
+++ docs/source/tutorial/genesis.sh
@@ -4,6 +4,6 @@ mkdir /home/vagrant/sawtooth
 mkdir /home/vagrant/sawtooth/keys
 mkdir /home/vagrant/sawtooth/data
 mkdir /home/vagrant/sawtooth/logs
-./bin/sawtooth keygen base000 --key-dir /home/vagrant/sawtooth/keys
-./bin/sawtooth admin poet0-genesis --keyfile /home/vagrant/sawtooth/keys/base000.wif -F mktplace.transactions.market_place -F ledger.transaction.integer_key
+/project/sawtooth-core/bin/sawtooth keygen base000 --key-dir /home/vagrant/sawtooth/keys
+/project/sawtooth-core/bin/sawtooth admin poet0-genesis --keyfile /home/vagrant/sawtooth/keys/base000.wif -F mktplace.transactions.market_place -F ledger.transaction.integer_key
 echo {\"InitialConnectivity\": 0} > /home/vagrant/sawtooth/v0.json
