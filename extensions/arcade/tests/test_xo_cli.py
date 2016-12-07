328d23153494de053a842256e6cd1cce9c63d19f
Andrea Gunderson <andreax.gunderson@intel.com>
2016-12-07 12:07:18 -0600

    Update arcade tests to expect already running validators and add arcade test suite
    Signed-off-by: Andrea Gunderson <andreax.gunderson@intel.com>

--- extensions/arcade/tests/test_xo_cli.py
+++ extensions/arcade/tests/test_xo_cli.py
@@ -26,11 +26,9 @@ from StringIO import StringIO
 
 from sawtooth.exceptions import ClientException
 from sawtooth_xo import xo_cli
-from txnintegration.validator_network_manager import get_default_vnm
 
-ENABLE_INTEGRATION_TESTS = False
-if os.environ.get("ENABLE_INTEGRATION_TESTS", False) == "1":
-    ENABLE_INTEGRATION_TESTS = True
+RUN_TEST_SUITES = True \
+    if os.environ.get("RUN_TEST_SUITES", False) == "1" else False
 
 
 @contextmanager
@@ -60,36 +58,8 @@ def std_output():
         sys.stderr = saved_err
 
 
-@unittest.skipUnless(ENABLE_INTEGRATION_TESTS, "integration test")
+@unittest.skipUnless(RUN_TEST_SUITES, "Must be run in a test suites")
 class TestXoCli(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.vnm = None
-        try:
-            if 'TEST_VALIDATOR_URL' in os.environ:
-                cls.url = os.environ['TEST_VALIDATOR_URL']
-            else:
-                overrides = {
-                    "TransactionFamilies": ['sawtooth_xo'],
-                }
-                cls.vnm = get_default_vnm(5, overrides=overrides)
-                cls.vnm.do_genesis()
-                cls.vnm.launch()
-                # the url of the initial validator
-                cls.url = cls.vnm.urls()[0] + '/'
-        except:
-            if cls.vnm is not None:
-                cls.vnm.shutdown()
-                cls.vnm = None
-            raise
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.vnm is not None:
-            cls.vnm.shutdown(archive_name='TestXoCli')
-        else:
-            print "No Validator data and logs to preserve."
-
     def _game_name(self):
         return 'game' + \
                ''.join(random.choice(string.digits) for i in range(10))
