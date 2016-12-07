69d5b2c8b9979c057b469abbe980a8cec1b7aad9
Andrea Gunderson <andreax.gunderson@intel.com>
2016-12-07 12:07:18 -0600

    Update arcade tests to expect already running validators and add arcade test suite
    Signed-off-by: Andrea Gunderson <andreax.gunderson@intel.com>

--- extensions/arcade/tests/test_battleship.py
+++ extensions/arcade/tests/test_battleship.py
@@ -5,43 +5,13 @@ import random
 import string
 
 from sawtooth_battleship import battleship_cli
-from txnintegration.validator_network_manager import get_default_vnm
 
-ENABLE_INTEGRATION_TESTS = False
-if os.environ.get("ENABLE_INTEGRATION_TESTS", False) == "1":
-    ENABLE_INTEGRATION_TESTS = True
+RUN_TEST_SUITES = True \
+    if os.environ.get("RUN_TEST_SUITES", False) == "1" else False
 
 
-@unittest.skipUnless(ENABLE_INTEGRATION_TESTS, "integration test")
+@unittest.skipUnless(RUN_TEST_SUITES, "Must be run in a test suites")
 class TestBattleshipCommands(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.vnm = None
-        try:
-            if 'TEST_VALIDATOR_URL' in os.environ:
-                cls.url = os.environ['TEST_VALIDATOR_URL']
-            else:
-                overrides = {
-                    "TransactionFamilies": ['sawtooth_battleship'],
-                }
-                cls.vnm = get_default_vnm(5, overrides=overrides)
-                cls.vnm.do_genesis()
-                cls.vnm.launch()
-                # the url of the initial validator
-                cls.url = cls.vnm.urls()[0] + '/'
-        except:
-            if cls.vnm is not None:
-                cls.vnm.shutdown()
-                cls.vnm = None
-            raise Exception("Validators didn't start up correctly.")
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.vnm is not None:
-            cls.vnm.shutdown(archive_name='TestBattleshipCli')
-        else:
-            print "No Validator data and logs to preserve."
 
     def _clean_data_and_key_files(self, user1, user2):
         home_dir = os.path.expanduser("~")
