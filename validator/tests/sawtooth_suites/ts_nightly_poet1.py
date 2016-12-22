a86df6c2d59f990f7c02aa52b2d54c3374b148fd
nickdrozd <drozd@bitwise.io>
2016-12-22 13:55:02 -0600

    Add class for test suite setup and teardown
    Signed-off-by: nickdrozd <drozd@bitwise.io>

--- validator/tests/sawtooth_suites/ts_nightly_poet1.py
+++ validator/tests/sawtooth_suites/ts_nightly_poet1.py
@@ -13,17 +13,11 @@
 # limitations under the License.
 # ------------------------------------------------------------------------------
 import logging
-import time
-import os
-import json
 import traceback
 import unittest
 
-from sawtooth.manage.node import NodeArguments
-from sawtooth.manage.subproc import SubprocessNodeController
-from sawtooth.manage.wrap import WrappedNodeController
-from txnintegration.utils import Progress
-from txnintegration.utils import TimeOut
+from sawtooth_test_suite import SawtoothTestSuite
+
 from integration.test_integration import TestIntegration
 from integration.test_convergence import TestConvergence
 from integration.test_validator_restart_restore \
@@ -36,61 +30,13 @@ from integration.test_sawtooth_stats import TestSawtoothStats
 LOGGER = logging.getLogger(__name__)
 
 
-class Poet1NightlyTestSuite(unittest.TestCase):
-    def _do_teardown(self):
-        print 'destroying', str(self.__class__.__name__)
-        if hasattr(self, '_node_ctrl') and self._node_ctrl is not None:
-            # Shut down the network
-            with Progress("terminating network") as p:
-                for node_name in self._node_ctrl.get_node_names():
-                    self._node_ctrl.stop(node_name)
-                to = TimeOut(16)
-                while len(self._node_ctrl.get_node_names()) > 0:
-                    if to.is_timed_out():
-                        break
-                    time.sleep(1)
-                    p.step()
-            # force kill anything left over
-            for node_name in self._node_ctrl.get_node_names():
-                try:
-                    print "%s still 'up'; sending kill..." % node_name
-                    self._node_ctrl.kill(node_name)
-                except Exception as e:
-                    print e.message
-            self._node_ctrl.archive(self.__class__.__name__)
-            self._node_ctrl.clean()
-
-    def _do_setup(self):
-        # give defaults to teardown vars
-        self._node_ctrl = None
-        print 'creating', str(self.__class__.__name__)
-        # set up our nodes (suite-internal interface)
-        self._node_ctrl = WrappedNodeController(SubprocessNodeController())
-        cfg = {"LedgerType": "poet1"}
-        temp_dir = self._node_ctrl.get_data_dir()
-        file_name = os.path.join(temp_dir, "config.js")
-        with open(file_name, 'w') as config:
-            config.write(json.dumps(cfg))
-
-        self._nodes = [
-            NodeArguments('v%s' % i, 8800 + i, 9000 + i,
-                          config_files=[file_name]) for i in range(5)]
-        # set up our urls (external interface)
-        self.urls = ['http://localhost:%s' % x.http_port for x in self._nodes]
-        # Make genesis block
-        print 'creating genesis block...'
-        self._nodes[0].genesis = True
-
-        self._node_ctrl.create_genesis_block(self._nodes[0])
-        # Launch network (node zero will trigger bootstrapping)
-        print 'launching network...'
-        for x in self._nodes:
-            self._node_ctrl.start(x)
-
+class Poet1NightlyTestSuite(SawtoothTestSuite):
     def test_suite(self):
+        cfg = {"LedgerType": "poet1"}
+
         success = False
         try:
-            self._do_setup()
+            self._do_setup(cfg)
             urls = self.urls
             suite = unittest.TestSuite()
             # test_bootstrap will allow the validators to all come on line
