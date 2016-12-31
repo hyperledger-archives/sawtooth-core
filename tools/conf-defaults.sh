3fc35d58c11d1e47ab0c83f011ab4b22aa5a925a
Shawn T. Amundson <amundson@bitwise.io>
2016-12-31 17:32:27 -0600

    Install zmq via packages file instead of plugin
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- tools/conf-defaults.sh
+++ tools/conf-defaults.sh
@@ -2,7 +2,7 @@
 
 [[ -z "$SETUP_SAWTOOTH_ENVIRONMENT" ]] && export SETUP_SAWTOOTH_ENVIRONMENT=yes
 [[ -z "$SETUP_SAWTOOTH_PATH" ]] && export SETUP_SAWTOOTH_PATH=no
-[[ -z "$PLUGINS" ]] && export PLUGINS="setup_homedir build_ubuntu_deps install_ubuntu_deps install_sphinx install_lmdb install_grpc install_bitcoin install_zmq install_aiohttp"
+[[ -z "$PLUGINS" ]] && export PLUGINS="setup_homedir build_ubuntu_deps install_ubuntu_deps install_sphinx install_lmdb install_grpc install_bitcoin install_aiohttp"
 
 function get_vagrant_user() {
     for user in vagrant ubuntu
--- tools/plugins/install_zmq.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-apt-get update && apt-get install -y \
-    python3-zmq \
-    libzmq-dev
