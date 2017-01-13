0c98429b5cbb519bbab0a8e1529fbecd586fe0f7
Zac Delventhal <zac@bitwise.io>
2017-01-13 15:38:15 -0600

    Add POST /batches route to REST Api
    Add route to allow REST Api to accept Protobuf binaries over HTTP
    and forward batches on to validator.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- rest_api/sawtooth_rest_api/routes.py
+++ rest_api/sawtooth_rest_api/routes.py
@@ -14,12 +14,27 @@
 # ------------------------------------------------------------------------------
 import asyncio
 from aiohttp import web
+
+from sawtooth_sdk.client.future import FutureTimeoutError
 from sawtooth_sdk.client.stream import Stream
+from sawtooth_validator.protobuf.validator_pb2 import Message
 
 
 class Routes(object):
     def __init__(self, stream_url):
         self._stream = Stream(stream_url)
+
+    def _try_future_content(self, future):
+        timeout = 5
+        timeout_msg = 'Could not reach validator, validator timed out'
+
+        try:
+            content = future.result(timeout=timeout).content
+        except FutureTimeoutError as e:
+            print(str(e))
+            raise web.HTTPGatewayTimeout(reason=timeout_msg)
+
+        return content
 
     @asyncio.coroutine
     def hello(self, request):
@@ -28,11 +43,37 @@ class Routes(object):
 
     @asyncio.coroutine
     def stream(self, request):
-        # will need to figure out a way to handle the case where the Validator
-        # is not running so no result will be returned
         text = "Connecting to Validator \n"
         future = self._stream.send(
             message_type='client/get',
             content=b"Temp Data")
-        extra = future.result().content
+        extra = self._try_future_content(future)
         return web.Response(text=text + str(extra) + "\n")
+
+    @asyncio.coroutine
+    def batches(self, request):
+        """
+        Takes a Protobuf binary from an HTTP Post, and sends it
+        to the Validator
+        """
+        mime_type = 'application/octet-stream'
+        type_msg = 'Expected an octet-stream encoded Protobuf binary'
+        type_error = web.HTTPBadRequest(reason=type_msg)
+
+        if request.headers['Content-Type'] != mime_type:
+            return type_error
+
+        payload = yield from request.read()
+
+        if type(payload) is not bytes:
+            return type_error
+
+        future = self._stream.send(
+            message_type=Message.CLIENT_BATCH_SUBMIT_REQUEST,
+            content=payload
+        )
+
+        validator_response = self._try_future_content(future)
+
+        # TODO: Update decode to serializing protobuf once merged
+        return web.json_response(validator_response.decode('utf-8'))
