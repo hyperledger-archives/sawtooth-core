378d0ac0ee9cb18d77ea78292f97e83163867d76
Shawn T. Amundson <amundson@bitwise.io>
2017-01-16 19:34:05 -0600

    Add argparse handling for tp_intkey_python
    Adds endpoint and verbose handling.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- sdk/examples/intkey_python/sawtooth_intkey/processor/main.py
+++ sdk/examples/intkey_python/sawtooth_intkey/processor/main.py
@@ -15,13 +15,72 @@
 
 import hashlib
 import sys
+import argparse
+import logging
+
+from colorlog import ColoredFormatter
 
 from sawtooth_sdk.processor.core import TransactionProcessor
 from sawtooth_intkey.processor.handler import IntkeyTransactionHandler
 
 
+def create_console_handler(verbose_level):
+    clog = logging.StreamHandler()
+    formatter = ColoredFormatter(
+        "%(log_color)s[%(asctime)s %(levelname)-8s%(module)s]%(reset)s "
+        "%(white)s%(message)s",
+        datefmt="%H:%M:%S",
+        reset=True,
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red',
+        })
+
+    clog.setFormatter(formatter)
+
+    if verbose_level == 0:
+        clog.setLevel(logging.WARN)
+    elif verbose_level == 1:
+        clog.setLevel(logging.INFO)
+    else:
+        clog.setLevel(logging.DEBUG)
+
+    return clog
+
+
+def init_console_logging(verbose_level=2):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    if verbose_level > 0:
+        logger.addHandler(create_console_handler(verbose_level))
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('endpoint',
+                        nargs='?',
+                        default='localhost:40000',
+                        help='Endpoint for the validator connection')
+    parser.add_argument('-v', '--verbose',
+                        action='count',
+                        default=0,
+                        help='Increase output sent to stderr')
+
+    return parser.parse_args(args)
+
+
 def main(args=sys.argv[1:]):
-    processor = TransactionProcessor(url=args[0])
+    opts = parse_args(args)
+
+    init_console_logging(verbose_level=opts.verbose)
+
+    processor = TransactionProcessor(url=opts.endpoint)
 
     # The prefix should eventually be looked up from the
     # validator's namespace registry.
