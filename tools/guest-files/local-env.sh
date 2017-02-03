c0d91c3775f76124d5a3d5d7dc630533f43ca989
Cian Montgomery <cian.montgomery@intel.com>
2017-02-03 15:43:31 -0800

    Fix the signing module path for the stl environment setup.
    Signed-off-by: Cian Montgomery <cian.montgomery@intel.com>

--- tools/guest-files/local-env.sh
+++ tools/guest-files/local-env.sh
@@ -4,7 +4,7 @@
 
 PYTHONPATH=/project/sawtooth-core/core
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/signing
-PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/signing/build/lib.linux-x86_64-2.7
+PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/signing/build/lib.linux-x86_64-3.5
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/validator
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/extensions/arcade
 PYTHONPATH=$PYTHONPATH:/project/sawtooth-core/sdk/python
