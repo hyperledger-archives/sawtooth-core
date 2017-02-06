25e9cc1b0d3b0518cc6dfaf22704c314f3e928cf
Boyd Johnson <boydx.johnson@intel.com>
2017-02-06 07:41:29 -0600

    Implement asynchronous processing dispatcher
    The dispatcher has handlers added to it for message types.
    If it has multiple handlers for the same message type it executes
    the handlers in the order that they were added, asynchronously.
    Handlers implement the Handler abstract base class and return
    a HandlerResult that must have a HandlerStatus.
    
    Added unit test that tests that no matter what happens inside
    a particular handler -- in this case time.sleep of a variable
    amount of time -- the identity and message that were added to
    the dispatcher are returned to the zmq interface.
    
    Signed-off-by: Boyd Johnson <boydx.johnson@intel.com>

--- validator/sawtooth_validator/server/dispatch.py
+++ validator/sawtooth_validator/server/dispatch.py
@@ -12,105 +12,196 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------------
-
-# pylint: disable=import-error,no-name-in-module
-# needed for google.protobuf import
-import queue
+import abc
+import enum
+from functools import partial
+import hashlib
 import logging
+from threading import Condition
+from threading import Thread
+import queue
+import uuid
 
-from threading import Thread, Condition
-from google.protobuf.message import DecodeError
+from sawtooth_validator.server.interconnect import get_enum_name
+from sawtooth_validator.protobuf import validator_pb2
 
-from sawtooth_validator.protobuf.batch_pb2 import Batch
-from sawtooth_validator.protobuf.block_pb2 import Block
-from sawtooth_validator.journal.completer import Completer
 LOGGER = logging.getLogger(__name__)
+
+
+def _gen_message_id():
+    return hashlib.sha512(uuid.uuid4().hex.encode()).hexdigest()
 
 
 class Dispatcher(Thread):
     def __init__(self):
-        super(Dispatcher, self).__init__()
-        self.on_batch_received = None
-        self.on_block_received = None
-        self.on_block_requested = None
-        self.incoming_msg_queue = None
-        self.completer_queue = queue.Queue()
-        self.completer_conditon = Condition()
-        self.condition = None
-        self._exit = False
-        self.completer = None
+        super().__init__()
+        self._msg_type_handlers = {}
+        self._in_queue = queue.Queue()
+        self._send_message = None
+        self._message_information = {}
+        self._condition = Condition()
+        self.daemon = True
 
-    def create_completer(self):
-        self.completer = Completer(self.on_block_received,
-                                   self.completer_conditon,
-                                   self.completer_queue)
-        self.completer.start()
+    def set_send_message(self, send_message):
+        self._send_message = send_message
 
-    def set_incoming_msg_queue(self, msg_queue):
-        self.incoming_msg_queue = msg_queue
+    def dispatch(self, identity, message):
+        if message.message_type in self._msg_type_handlers:
+            message_id = _gen_message_id()
+            self._message_information[message_id] = (
+                identity,
+                message,
+                _ManagerCollection(
+                    self._msg_type_handlers[message.message_type])
+            )
+            self._in_queue.put_nowait(message_id)
+        else:
+            LOGGER.info("received a message of type %s "
+                        "from %s but have no handler for that type",
+                        get_enum_name(message.message_type),
+                        identity)
 
-    def set_condition(self, condition):
-        self.condition = condition
+    def add_handler(self, message_type, handler, executor):
+        if not isinstance(handler, Handler):
+            raise ValueError("%s is not a Handler subclass", handler)
+        if message_type not in self._msg_type_handlers:
+            self._msg_type_handlers[message_type] = [
+                _HandlerManager(executor, handler)]
+        else:
+            self._msg_type_handlers[message_type].append(
+                _HandlerManager(executor, handler))
 
-    def set_on_block_received(self, on_block_received_func):
-        self.on_block_received = on_block_received_func
+    def _process(self, message_id):
+        with self._condition:
+            identity, message, collection = self._message_information[
+                message_id]
+        try:
+            handler_manager = next(collection)
+            future = handler_manager.execute(identity, message.content)
+            future.add_done_callback(partial(self._determine_next, message_id))
+        except IndexError:
+            # IndexError is raised if done with handlers
+            with self._condition:
+                del self._message_information[message_id]
 
-    def set_on_batch_received(self, on_batch_received_func):
-        self.on_batch_received = on_batch_received_func
+    def _determine_next(self, message_id, future):
+        if future.result().status == HandlerStatus.DROP:
+            with self._condition:
+                del self._message_information[message_id]
 
-    def set_on_block_requested(self, on_block_requested_func):
-        self.on_block_requested = on_block_requested_func
+        elif future.result().status == HandlerStatus.PASS:
+            self._process(message_id)
 
-    def stop(self):
-        self._exit = True
-        self.completer.stop()
-        with self.completer_conditon:
-            self.completer_conditon.notify_all()
+        elif future.result().status == HandlerStatus.RETURN_AND_PASS:
+            with self._condition:
+                ident, original_message, _ = self._message_information[
+                    message_id]
+
+            message = validator_pb2.Message(
+                content=future.result().message_out.SerializeToString(),
+                correlation_id=original_message.correlation_id,
+                message_type=future.result().message_type)
+
+            self._send_message(msg=message,
+                               identity=ident)
+            self._process(message_id)
+
+        elif future.result().status == HandlerStatus.RETURN:
+            with self._condition:
+                ident, original_message, _ = self._message_information[
+                    message_id]
+
+                del self._message_information[message_id]
+
+            message = validator_pb2.Message(
+                content=future.result().message_out.SerializeToString(),
+                correlation_id=original_message.correlation_id,
+                message_type=future.result().message_type)
+
+            self._send_message(msg=message,
+                               identity=ident)
+        with self._condition:
+            if len(self._message_information) == 0:
+                self._condition.notify()
 
     def run(self):
-        if self.incoming_msg_queue is None or self.condition is None:
-            LOGGER.warning("Dispatcher can not be used if incoming_msg_queue "
-                           "or condition is not set.")
-            return
-
         while True:
-            if self._exit:
-                return
+            msg_id = self._in_queue.get()
+            if msg_id == -1:
+                break
+            self._process(msg_id)
 
-            try:
-                request = self.incoming_msg_queue.get(block=False)
-                if request.content_type == "Block":
-                    try:
-                        block = Block()
-                        block.ParseFromString(request.content)
-                        LOGGER.debug("Block given to Completer %s",
-                                     block.header_signature)
-                        self.completer_queue.put_nowait(block)
-                        with self.completer_conditon:
-                            self.completer_conditon.notify_all()
-                    except DecodeError as e:
-                        # what to do with a bad msg
-                        LOGGER.warning("Problem decoding GossipMessage for "
-                                       "Block, %s", e)
+    def stop(self):
+        self._in_queue.put_nowait(-1)
 
-                elif request.content_type == "Batch":
-                    try:
-                        batch = Batch()
-                        batch.ParseFromString(request.content)
-                        self.completer.add_batch(batch)
-                        self.on_batch_received(batch)
+    def block_until_complete(self):
+        """Blocks until no more messages are in flight,
+        useful for unit tests.
+        """
+        with self._condition:
+            if len(self._message_information) > 0:
+                self._condition.wait()
 
-                    except DecodeError as e:
-                        LOGGER.warning("Problem decoding GossipMessage for "
-                                       "Batch, %s", e)
 
-                elif request.content_type == "BlockRequest":
-                    block_id = str(request.content, "utf-8")
-                    self.on_block_requested(block_id)
+class _HandlerManager(object):
+    def __init__(self, executor, handler):
+        """
+        :param executor: concurrent.futures.Executor
+        :param handler: Handler subclass
+        """
+        self._executor = executor
+        self._handler = handler
 
-                elif request.content_type == "Test":
-                    LOGGER.debug("Dispatch Handle Test")
+    def execute(self, identity, message):
+        return self._executor.submit(self._handler.handle, identity, message)
 
-            except queue.Empty:
-                with self.condition:
-                    self.condition.wait()
+
+class _ManagerCollection(object):
+    """Wraps a list of _HandlerManagers and
+    keeps track of which handler_manager is next
+    """
+    def __init__(self, handler_managers):
+        self._chain = handler_managers
+        self._index = 0
+
+    def __next__(self):
+        result = self._chain[self._index]
+        self._index += 1
+        return result
+
+
+class HandlerResult(object):
+    def __init__(self, status, message_out=None, message_type=None):
+        """
+        :param status HandlerStatus: the status of the handler's processing
+        :param message_out protobuf Python class:
+        :param message_type: validator_pb2.Message.* enum value
+        """
+        self.status = status
+        self.message_out = message_out
+        self.message_type = message_type
+
+
+class HandlerStatus(enum.Enum):
+    DROP = 1  # Do no further processing on the message
+    RETURN = 2  # Send the message out. Could be because of error condition
+    RETURN_AND_PASS = 3  # Send a message out and process the next handler
+    PASS = 4  # Send the message to the next handler
+
+
+class Handler(object, metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def handle(self, identity, message_content):
+        """
+
+        :param identity: zmq identity set on the zmq socket
+                         could be a pubkey and used in a
+                         authentication handler.
+        :param message_content: The bytes to be deserialized
+                                into a protobuf python class
+        :return HandlerResult: The status of the handling
+                                and optionally the message
+                                and message_type to send out
+        """
+        raise NotImplementedError()
