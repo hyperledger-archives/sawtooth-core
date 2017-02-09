4dee2cb81fa805b49ad14b3075402ee722794485
Nick Drozd <nicholasx.drozd@intel.com>
2017-02-09 13:53:06 -0600

    Add intkey test verifier
    Signed-off-by: Nick Drozd <nicholasx.drozd@intel.com>

--- integration/sawtooth_integration/tests/test_intkey_smoke.py
+++ integration/sawtooth_integration/tests/test_intkey_smoke.py
@@ -14,20 +14,20 @@
 # ------------------------------------------------------------------------------
 
 import unittest
-import subprocess
 import time
 import logging
-import operator
+import operator # used by verifier
+# -- used by rest_api callers --
 import urllib.request
 import urllib.error
 import json
-import shlex
 from base64 import b64decode
 
 import cbor
+# ------------------------------
 
 from sawtooth_integration.intkey.intkey_message_factory \
-    import IntkeyMessageFactory
+    import IntkeyMessageFactory # used by verifier
 
 
 LOGGER = logging.getLogger(__name__)
@@ -57,17 +57,18 @@ class TestIntkeySmoke(unittest.TestCase):
         6. Send the batch of valid transactions, again altering the state.
         '''
 
-        # batch file names (strings)
-        populate, valid_txns, invalid_txns = _make_txn_files()
+        self.verifier = IntkeyTestVerifier()
 
-        self.assert_empty_state()
+        populate, valid_txns, invalid_txns = self.make_txn_batches()
+
+        self.verify_empty_state()
 
         # 2/7/17: The validator seems to stop responding when sent
         # invalid txns (a bug). The invalid txns are commented out
         # so the test will pass for the moment, but they should be
         # be uncommented when that issue is addressed.
 
-        cmds = (
+        batches = (
             populate,
             valid_txns,
             # invalid_txns,
@@ -78,36 +79,42 @@ class TestIntkeySmoke(unittest.TestCase):
 
         how_many_updates = 0
 
-        for cmd in cmds:
-            _intkey_load(cmd)
-            if cmd == valid_txns:
+        for batch in batches:
+            if batch == valid_txns:
                 how_many_updates += 1
-            self.assert_state_after_n_updates(how_many_updates)
+            self.post_and_verify(batch, how_many_updates)
 
     # assertions
 
-    def assert_state_after_n_updates(self, num):
-        expected_state = _state_after_n_updates(num)
+    def post_and_verify(self, batch, how_many_updates):
+        LOGGER.info('Posting batch')
+        _post_batch(batch)
+        time.sleep(1)
+        self.verify_state_after_n_updates(how_many_updates)
+
+    def verify_state_after_n_updates(self, num):
+        LOGGER.debug('Verifying state after {} updates'.format(num))
+        expected_state = self.verifier.state_after_n_updates(num)
         actual_state = _get_data()
+        self.assertEqual(expected_state, actual_state)
 
-        self.assertEqual(
-            expected_state,
-            actual_state)
+    def verify_empty_state(self):
+        LOGGER.debug('Verifying empty state')
+        self.assertRaises(urllib.error.HTTPError, _get_state)
 
-    def assert_empty_state(self):
-        state = _get_state()
-        self.assertEqual('404', state)
+    # utilities
 
-# utilities
+    def make_txn_batches(self):
+        LOGGER.debug('Making txn batches')
+        batches = self.verifier.make_txn_batches()
+        return batches
 
-def _intkey_load(file):
-    LOGGER.info('loading {}'.format(file))
-    load_cmd = shlex.split('intkey load -f {} -U validator:40000'.format(file))
-    subprocess.run(load_cmd,
-                   stdout=subprocess.PIPE,
-                   stderr=subprocess.PIPE,
-                   check=True)
-    time.sleep(1)
+# rest_api calls
+
+def _post_batch(batch):
+    headers = {'Content-Type': 'application/octet-stream'}
+    response = _query_rest_api('/batches', data=batch, headers=headers)
+    return response
 
 def _get_data():
     state = _get_state()
@@ -118,101 +125,60 @@ def _get_data():
 
 def _get_state():
     root = _get_root()
-    try:
-        response = _query_rest_api('/{}'.format(root))
-    except urllib.error.HTTPError:
-        return '404'
+    response = _query_rest_api('/state/{}'.format(root))
     state = json.loads(response)['entries']
     return state
 
 def _get_root():
-    response = _query_rest_api()
+    response = _query_rest_api('/state')
     data = json.loads(response)
     root = data['merkleRoot']
     return root
 
-def _query_rest_api(suffix=''):
-    url = 'http://rest_api:8080/state' + suffix
-    request = urllib.request.Request(url)
+def _query_rest_api(suffix='', data=None, headers={}):
+    url = 'http://rest_api:8080' + suffix
+    request = urllib.request.Request(url, data, headers)
     response = urllib.request.urlopen(request).read().decode('utf-8')
     return response
 
-# expected state
+# separate file?
 
-def _state_after_n_updates(num):
-    ops = {
-        'inc': operator.add,
-        'dec': operator.sub
-    }
+class IntkeyTestVerifier:
+    def __init__(self,
+                 valid=('cow', 'pig', 'sheep', 'goat', 'horse'),
+                 invalid=('lark', 'thrush', 'jay', 'wren', 'finch'),
+                 verbs=('inc', 'inc', 'dec', 'inc', 'dec'),
+                 incdec=(1, 2, 3, 5, 8),
+                 initial=(415, 325, 538, 437, 651)):
+        self.valid = valid
+        self.invalid = invalid
+        self.verbs = verbs
+        self.incdec = incdec
+        self.initial = initial
+        self.sets = ['set' for _ in range(len(self.initial))]
 
-    expected_values = [
-        ops[verb](init, (val * num))
-        for verb, init, val
-        in zip(_verbs(), _initial_values(), _inc_dec_values())
-    ]
+    def make_txn_batches(self):
+        def make_batch(verbs, words, values):
+            txns = zip(verbs, words, values)
+            batch = IntkeyMessageFactory().create_batch(txns)
+            return batch
 
-    return {word: val for word, val in zip(_valid_words(), expected_values)}
+        populate = make_batch(self.sets, self.valid, self.initial)
+        valid_txns = make_batch(self.verbs, self.valid, self.incdec)
+        invalid_txns = make_batch(self.verbs, self.invalid, self.incdec)
 
-# transaction constants
+        return populate, valid_txns, invalid_txns
 
-def _valid_words():
-    return 'cow', 'pig', 'sheep', 'goat', 'horse'
+    def state_after_n_updates(self, num):
+        ops = {
+            'inc': operator.add,
+            'dec': operator.sub
+        }
 
-def _invalid_words():
-    return 'lark', 'thrush', 'jay', 'wren', 'finch'
+        expected_values = [
+            ops[verb](init, (val * num))
+            for verb, init, val
+            in zip(self.verbs, self.initial, self.incdec)
+        ]
 
-def _initial_values():
-    return 415, 325, 538, 437, 651
-
-def _inc_dec_values():
-    return 1, 2, 3, 5, 8
-
-def _verbs():
-    return 'inc', 'inc', 'dec', 'inc', 'dec'
-
-# intkey batch creation
-
-def _make_txn_files():
-    # file names
-    prefix = '/tmp/intkey_smoke_test_'
-    populate = prefix + 'populate'
-    valid_txns = prefix + 'valid'
-    invalid_txns = prefix + 'invalid'
-
-    valid_words = _valid_words()
-    invalid_words = _invalid_words()
-    initial_values = _initial_values()
-    verbs = _verbs()
-    sets = ('set' for _ in range(len(initial_values)))
-    inc_dec_values = _inc_dec_values()
-
-    _make_batch_file(
-        sets,
-        valid_words,
-        initial_values,
-        populate)
-
-    _make_batch_file(
-        verbs,
-        valid_words,
-        inc_dec_values,
-        valid_txns)
-
-    _make_batch_file(
-        verbs,
-        invalid_words,
-        inc_dec_values,
-        invalid_txns)
-
-    # return file names
-    return populate, valid_txns, invalid_txns
-
-
-def _make_batch_file(verbs, words, values, file_name):
-    txns = zip(verbs, words, values)
-
-    batch = IntkeyMessageFactory().create_batch(txns)
-
-    with open(file_name, 'w+b') as file:
-        LOGGER.debug('writing to {}'.format(file_name))
-        file.write(batch)
+        return {word: val for word, val in zip(self.valid, expected_values)}
