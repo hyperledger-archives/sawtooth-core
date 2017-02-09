f10aa3117be27256f8fd8aad7b702f69ca6bf4c4
Shawn T. Amundson <amundson@bitwise.io>
2017-02-08 19:19:42 -0600

    Fix linting error
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- validator/gossip/signed_object.py
+++ validator/gossip/signed_object.py
@@ -231,7 +231,8 @@ class SignedObject(object):
             recovered_id = self.OriginatorID
             if self.public_key is None and originatorid is None:
                 logger.warning("Identifier %s of SignedObject has no public "
-                    "key and addr used to verify signature", self.Identifier)
+                               "key and addr used to verify signature",
+                               self.Identifier)
             return (self.public_key is None or self.public_key ==
                     self.originator_public_key) and \
                 (originatorid is None or recovered_id == originatorid)
