de2290075781b969022ef1649880e02c5d13713e
Shawn T. Amundson <amundson@bitwise.io>
2017-02-12 16:57:10 -0600

    Remove the TopologicalSorter from parallel scheduler
    The parallel scheduler has to determine this dependency calculation
    dynamically and therefore this static sorter class is not of actual
    use in determining the order of transactions.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- validator/tests/unit3/test_scheduler/tests.py
+++ validator/tests/unit3/test_scheduler/tests.py
@@ -22,7 +22,6 @@ import sawtooth_validator.protobuf.transaction_pb2 as transaction_pb2
 
 from sawtooth_validator.execution.context_manager import ContextManager
 from sawtooth_validator.execution.scheduler_parallel import PredecessorTree
-from sawtooth_validator.execution.scheduler_parallel import TopologicalSorter
 from sawtooth_validator.execution.scheduler_serial import SerialScheduler
 from sawtooth_validator.database import dict_database
 
@@ -401,22 +400,3 @@ class TestPredecessorTree(unittest.TestCase):
         self.assertEqual(
             tree.find_read_predecessors(address_b),
             set(['txn3']))
-
-
-class TestTopologicalSorter(unittest.TestCase):
-
-    def test_topological_sorter(self):
-        sorter = TopologicalSorter()
-        sorter.add_relation('9', '2')
-        sorter.add_relation('3', '7')
-        sorter.add_relation('7', '5')
-        sorter.add_relation('5', '8')
-        sorter.add_relation('8', '6')
-        sorter.add_relation('4', '6')
-        sorter.add_relation('1', '3')
-        sorter.add_relation('7', '4')
-        sorter.add_relation('9', '5')
-        sorter.add_relation('2', '8')
-        self.assertEqual(
-            sorter.order(),
-            ['9', '2', '1', '3', '7', '5', '8', '4', '6'])
