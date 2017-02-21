3c3ef129b49798469fbac9dc94f2d24e7ee8bc7a
Shawn T. Amundson <amundson@bitwise.io>
2017-02-21 10:56:36 -0600

    Remove setup.py clean --all option
    This has been replaced by bin/clean_all.  The --all option was difficult
    to maintain.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- manage/setup.py
+++ manage/setup.py
@@ -73,16 +73,3 @@ setup(name='sawtooth-manage',
       url='http://www.intel.com',
       packages=find_packages(),
       entry_points={})
-
-if "clean" in sys.argv and "--all" in sys.argv:
-    directory = os.path.dirname(os.path.realpath(__file__))
-    for root, fn_dir, files in os.walk(directory):
-        for fn in files:
-            if fn.endswith(".pyc"):
-                os.remove(os.path.join(root, fn))
-    for filename in ['.coverage']:
-        if os.path.exists(os.path.join(directory, filename)):
-            os.remove(os.path.join(directory, filename))
-    shutil.rmtree(os.path.join(directory, "build"), ignore_errors=True)
-    shutil.rmtree(os.path.join(directory, "htmlcov"), ignore_errors=True)
-    shutil.rmtree(os.path.join(directory, "deb_dist"), ignore_errors=True)
