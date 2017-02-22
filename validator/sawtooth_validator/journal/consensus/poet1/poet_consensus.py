e4473169f83445897097676756d67c4405899ccf
Shawn T. Amundson <amundson@bitwise.io>
2017-02-22 17:13:41 -0600

    Fix poet1 pylint "import-error" for gossip imports
    This removes gossip imports for gossip.common and gossip.stats.  For
    common, it replaces it with the correct reference for null block
    identifier.  For stats, it removes the lines using stats since the
    API for 0.8 is likely to be substantially different.
    
    The stats were: LocalMeanTime, AggregateLocalMean, PopulationEstimate,
    ExpectedExpirationTime, and Duration.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- validator/sawtooth_validator/journal/consensus/poet1/poet_consensus.py
+++ validator/sawtooth_validator/journal/consensus/poet1/poet_consensus.py
@@ -815,14 +815,6 @@ class PoetConsensus(object):
             journal.maximum_blocks_to_keep,
             WaitTimer.certificate_sample_length)
 
-        # initialize stats specifically for the block chain journal
-        journal.JournalStats.add_metric(stats.Value('LocalMeanTime', 0))
-        journal.JournalStats.add_metric(stats.Value('AggregateLocalMean', 0))
-        journal.JournalStats.add_metric(stats.Value('PopulationEstimate', 0))
-        journal.JournalStats.add_metric(stats.Value('ExpectedExpirationTime',
-                                                    0))
-        journal.JournalStats.add_metric(stats.Value('Duration', 0))
-
         # initialize the block handlers
         poet_transaction_block.register_message_handlers(journal)
 
