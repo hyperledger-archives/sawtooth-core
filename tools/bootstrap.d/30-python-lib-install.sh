510c9ffbffbf49a8b46bdf660584a72b083584a1
Shawn T. Amundson <amundson@bitwise.io>
2017-02-21 20:47:35 -0600

    Remove requirements.txt-based pip installs
    With the migration to python 3, we no longer need python 2 pip
    installs.  Also, we prefer deb installs for dependencies.
    
    This leaves the pip3 install of pylint because pylint3 in 16.04
    seems broken on mehtods using async.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- tools/bootstrap.d/30-python-lib-install.sh
+++ tools/bootstrap.d/30-python-lib-install.sh
@@ -2,29 +2,4 @@
 
 set -e
 
-function install_requires {
-    cat $1 \
-        | grep -v "sawtooth-signing" \
-        | grep -v "sawtooth-core" \
-        | pip install -r /dev/stdin
-}
-
-# Packages for testing
-pip install -r /project/sawtooth-core/tools/requirements-testing.txt
 pip3 install pylint
-
-# sawtooth-core/signing
-(cd /project/sawtooth-core/signing/ && python setup.py egg_info)
-pip install -r /project/sawtooth-core/signing/sawtooth_signing.egg-info/requires.txt
-
-# sawtooth-core/core
-(cd /project/sawtooth-core/core/ && python setup.py egg_info)
-pip install -r /project/sawtooth-core/core/sawtooth_core.egg-info/requires.txt
-
-# sawtooth-core/validator
-(cd /project/sawtooth-core/validator/ && python setup.py egg_info)
-install_requires /project/sawtooth-core/validator/sawtooth_validator.egg-info/requires.txt
-
-# sawtooth-core/extensions/arcade
-(cd /project/sawtooth-core/extensions/arcade/ && python setup.py egg_info)
-install_requires /project/sawtooth-core/extensions/arcade/sawtooth_arcade.egg-info/requires.txt
