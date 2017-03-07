ff2f1856182509f4c88fb762f2d491d2e7dfeb96
Shawn T. Amundson <amundson@bitwise.io>
2017-03-07 16:04:31 -0600

    Load path config and exit on file/directory errors
    Adds PathConfig and the load_path_config(), which is called from
    validator's main().  Verifies that data_dir and log_dir exist and
    are writable.  Also verifies that the identity signing key can be
    loaded.
    
    This moves the use of load_identity_signing_key out of Validator's
    constructor and into main() so errors are caught earlier.
    
    Adds a command-line option --config-dir to validator.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- validator/sawtooth_validator/server/keys.py
+++ validator/sawtooth_validator/server/keys.py
@@ -15,15 +15,17 @@
 
 import logging
 import os
+import sys
 
 from sawtooth_signing import secp256k1_signer as signing
 
+from sawtooth_validator.exceptions import LocalConfigurationError
 
 LOGGER = logging.getLogger(__name__)
 
 
 def load_identity_signing_key(key_dir, key_name):
-    """Loads a private key from the key director, based on a validator's
+    """Loads a private key from the key directory, based on a validator's
     identity.
 
     Args:
@@ -35,16 +37,34 @@ def load_identity_signing_key(key_dir, key_name):
     """
     key_path = os.path.join(key_dir, '{}.wif'.format(key_name))
 
-    if os.path.exists(key_path):
-        LOGGER.debug('Found signing key %s', key_path)
+    if not os.path.exists(key_path):
+        raise LocalConfigurationError(
+            "No such signing key file: {}".format(key_path))
+    if not os.access(key_path, os.R_OK):
+        raise LocalConfigurationError(
+            "Key file is not readable: {}".format(key_path))
+
+    LOGGER.info('Loading signing key: %s', key_path)
+    try:
         with open(key_path, 'r') as key_file:
             wif_key = key_file.read().strip()
-            return signing.encode_privkey(
-                signing.decode_privkey(wif_key), 'hex')
-    else:
-        LOGGER.info('No signing key found. Generating %s', key_path)
-        priv_key = signing.generate_privkey()
-        with open(key_path, 'w') as key_file:
-            key_file.write(signing.encode_privkey(priv_key))
+    except IOError as e:
+        raise LocalConfigurationError(
+            "Could not load key file: {}".format(str(e)))
 
-        return signing.encode_privkey(priv_key, 'hex')
+    try:
+        decoded_key = signing.decode_privkey(wif_key)
+    except AssertionError:
+        # The underlying bitcoin library used by sawtooth_signing asserts to
+        # verify correctness of the format.  While we would not normally both
+        # log the error and raise an exception, in this case we may need the
+        # stacktrace to determine the root cause of the AssertionError, since
+        # there is no message provided as part of it.  We log it as debug,
+        # while the exception is probably handled by the caller as a fatal
+        # startup error.
+        LOGGER.debug(
+            "AssertionError while decoding wif key", exc_info=sys.exc_info())
+        raise LocalConfigurationError(
+            "Could not decode key contained in file (AssertionError): "
+            "{}".format(key_path))
+    return signing.encode_privkey(decoded_key, 'hex')
