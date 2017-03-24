dd2e0df6ef0b3e5ade9f61e10dd0b964f4ed361f
Adam Ludvik <ludvik@bitwise.io>
2017-03-24 13:40:43 -0500

    Include sawtooth repo
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- tools/bootstrap.d/20-package-update.sh
+++ tools/bootstrap.d/20-package-update.sh
@@ -3,6 +3,8 @@
 set -e
 
 if [ -f /etc/debian_version ]; then
+    echo "deb http://repo.sawtooth.me/ xenial universe" >> /etc/apt/sources.list
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6B58B1AC10FB5F63
     apt-get update -y
 elif [ -f /etc/redhat-release ]; then
     yum upgrade -y
