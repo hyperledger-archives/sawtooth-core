84d0b46d35c4071f1e7280a8b28dd937e83a931e
Adam Ludvik <ludvik@bitwise.io>
2017-04-14 15:48:53 -0500

    Update validator packaging and systemd files
    Reorganize packaging files to make it more clear which platform they are
    used for. Also update the systemd files to work with the current version
    of validator.
    
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- validator/packaging/create_package.d/30-package_arcade.ps1
+++
@@ -1,9 +0,0 @@
-# build and install sawtooth-arcade for packaging
-
-cd $build_dir\sawtooth-core\extensions\arcade
-python setup.py clean --all
-if ($lastexitcode -ne 0) { exit 1 }
-python setup.py build
-if ($lastexitcode -ne 0) { exit 1 }
-iex $build_command
-if ($lastexitcode -ne 0) { exit 1 }
