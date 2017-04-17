bb7e6bb984150648af89ba8e874b87bc694bb6c9
Andrea Gunderson <andreax.gunderson@intel.com>
2017-04-17 10:06:30 -0500

    Update workload generator and intkey workload to use rest api
    Also make the worklaod shutdown correctly.
    
    Signed-off-by: Andrea Gunderson <andreax.gunderson@intel.com>

--- sdk/python/sawtooth_sdk/workload/workload_generator.py
+++ sdk/python/sawtooth_sdk/workload/workload_generator.py
@@ -15,20 +15,20 @@
 import asyncio
 import logging
 import time
+import json
+import urllib.request as urllib
+from urllib.error import URLError, HTTPError
+from http.client import RemoteDisconnected
 
 from threading import Lock
 from collections import deque
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures._base import CancelledError
 
-from sawtooth_sdk.messaging.future import FutureTimeoutError
-from sawtooth_sdk.messaging.exceptions import ValidatorConnectionError
 from sawtooth_sdk.messaging.exceptions import WorkloadConfigurationError
-from sawtooth_sdk.protobuf.client_pb2 import ClientBatchStatusRequest
-from sawtooth_sdk.protobuf.client_pb2 import ClientBatchStatusResponse
-from sawtooth_sdk.protobuf.validator_pb2 import Message
 
-PendingBatch = namedtuple('PendingBatch', ['id', 'stream'])
+PendingBatch = namedtuple('PendingBatch', ['id', 'url'])
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
@@ -73,64 +73,69 @@ class WorkloadGenerator(object):
 
     @asyncio.coroutine
     def _simulator_loop(self):
+        while True:
+            yield from asyncio.sleep(self._rate)
+            with self._lock:
+                now = time.time()
+                delta = now - self._time_since_last_check
+                if delta >= self._display_frequency:
+                    sample =  \
+                        (self._submitted_batches_sample / delta)
+                    self._submitted_batch_samples.append(sample)
+                    LOGGER.warning(
+                        'Transaction submission rate for last sample '
+                        'period is %.2f tps',
+                        sample)
+                    sample =  \
+                        (self._committed_batches_sample / delta)
+                    self._committed_batch_samples.append(sample)
+                    LOGGER.warning(
+                        'Transaction commit rate for last sample period is'
+                        ' %.2f tps',
+                        sample)
+
+                    # We are going to only use at most the last 10 samples
+                    # to calculate the moving average
+                    if len(self._submitted_batch_samples) == 11:
+                        self._submitted_batch_samples.popleft()
+                        self._committed_batch_samples.popleft()
+
+                    LOGGER.warning(
+                        'Transaction submission rate for last %d sample(s)'
+                        ' is %.2f tps',
+                        len(self._submitted_batch_samples),
+                        sum(self._submitted_batch_samples) /
+                        len(self._submitted_batch_samples))
+                    LOGGER.warning(
+                        'Transaction commit rate for last %d sample(s)'
+                        ' is %.2f tps',
+                        len(self._committed_batch_samples),
+                        sum(self._committed_batch_samples) /
+                        len(self._committed_batch_samples))
+
+                    self._submitted_batches_sample = 0
+                    self._committed_batches_sample = 0
+                    self._time_since_last_check = now
+
+                self._number_of_outstanding_requests += 1
+                # If there are pending batchess, then pull off the first
+                # one.
+                batch = \
+                    self._pending_batches.popleft() \
+                    if len(self._pending_batches) > 0 else None
+                self.loop.run_in_executor(
+                    self.thread_pool, self._check_on_batch, batch)
+
+    def stop(self):
+        tasks = asyncio.Task.all_tasks(self.loop)
+        for task in tasks:
+            self.loop.call_soon_threadsafe(task.cancel)
         try:
-            while True:
-                yield from asyncio.sleep(self._rate)
-                with self._lock:
-                    now = time.time()
-                    delta = now - self._time_since_last_check
-                    if delta >= self._display_frequency:
-                        sample =  \
-                            (self._submitted_batches_sample / delta)
-                        self._submitted_batch_samples.append(sample)
-                        LOGGER.warning(
-                            'Transaction submission rate for last sample '
-                            'period is %.2f tps',
-                            sample)
-                        sample =  \
-                            (self._committed_batches_sample / delta)
-                        self._committed_batch_samples.append(sample)
-                        LOGGER.warning(
-                            'Transaction commit rate for last sample period is'
-                            ' %.2f tps',
-                            sample)
-
-                        # We are going to only use at most the last 10 samples
-                        # to calculate the moving average
-                        if len(self._submitted_batch_samples) == 11:
-                            self._submitted_batch_samples.popleft()
-                            self._committed_batch_samples.popleft()
-
-                        LOGGER.warning(
-                            'Transaction submission rate for last %d sample(s)'
-                            ' is %.2f tps',
-                            len(self._submitted_batch_samples),
-                            sum(self._submitted_batch_samples) /
-                            len(self._submitted_batch_samples))
-                        LOGGER.warning(
-                            'Transaction commit rate for last %d sample(s)'
-                            ' is %.2f tps',
-                            len(self._committed_batch_samples),
-                            sum(self._committed_batch_samples) /
-                            len(self._committed_batch_samples))
-
-                        self._submitted_batches_sample = 0
-                        self._committed_batches_sample = 0
-                        self._time_since_last_check = now
-
-                    self._number_of_outstanding_requests += 1
-                    # If there are pending batchess, then pull off the first
-                    # one.
-                    batch = \
-                        self._pending_batches.popleft() \
-                        if len(self._pending_batches) > 0 else None
-
-                    self.loop.run_in_executor(
-                        self.thread_pool, self._check_on_batch, batch)
-
-        except KeyboardInterrupt:
-            self._workload.on_will_stop()
-            self.loop.stop()
+            self.loop.run_until_complete(
+                asyncio.gather(*asyncio.Task.all_tasks()))
+        except CancelledError:
+            self.loop.call_soon_threadsafe(self.loop.stop)
+        self._workload.on_will_stop()
 
     def _check_on_batch(self, batch):
         """ If the batch is not None, then we will check its status and
@@ -139,15 +144,11 @@ class WorkloadGenerator(object):
 
             Args:
                 batch: PendingBatch named tuple that contains the batch id and
-                       the stream the batch was submitted to.
+                       the url the batch was submitted to.
         """
         if batch is not None:
-            status = self._get_batch_status(batch.id, batch.stream)
+            status = self._status_request([batch.id], batch.url)
             if status == "COMMITTED":
-                committed = True
-            else:
-                committed = False
-            if committed:
                 with self._lock:
                     self._committed_batches_sample += 1
                 self._workload.on_batch_committed(batch.id)
@@ -177,14 +178,14 @@ class WorkloadGenerator(object):
             self._validators.remove(validator)
         self._workload.on_validator_removed(validator)
 
-    def on_new_batch(self, batch_id, stream):
+    def on_new_batch(self, batch_id, url):
         """
         Called by the workload to let the workload_generator know that a new
         batch should be tracked.
 
         Args:
             batch_id: The ID for the new batch.
-            stream: The validator to which the batch has been submitted.
+            url: The rest_api to which the batch has been submitted.
 
         Returns:
             Nothing
@@ -193,30 +194,43 @@ class WorkloadGenerator(object):
             with self._lock:
                 self._submitted_batches_sample += 1
                 self._pending_batches.append(
-                    PendingBatch(id=batch_id, stream=stream))
+                    PendingBatch(id=batch_id, url=url))
 
-    def _get_batch_status(self, batch_id, stream):
-        """
-        Sends a ClientBatchStatusRequest to the stream that the batch was
-        submitted to
-        """
-        request = ClientBatchStatusRequest(batch_ids=[batch_id])
-        future = stream.send(
-            message_type=Message.CLIENT_BATCH_STATUS_REQUEST,
-            content=request.SerializeToString())
+    def _status_request(self, batch_id, url):
+        data = json.dumps(batch_id).encode()
+        headers = {'Content-Type': 'application/json'}
+        headers['Content-Length'] = '%d' % len(data)
+
+        request = urllib.Request(
+            url + '/batch_status',
+            data,
+            headers=headers,
+            method='POST')
+
         try:
-            result = future.result(timeout=0.5)
-            response = ClientBatchStatusResponse()
-            response.ParseFromString(result.content)
-            return ClientBatchStatusResponse.BatchStatus.Name(
-                response.batch_statuses[batch_id])
+            result = urllib.urlopen(request)
+            code, json_result = \
+                result.status, json.loads(result.read().decode())
 
-        except ValidatorConnectionError:
-            LOGGER.warning("The validator at %s is no longer connected. "
-                           "Removing Validator.", stream.url)
-            self._remove_unresponsive_validator(stream.url)
+            if code == 200 or code == 201 or code == 202:
+                status = list(json_result["data"].values())[0]
+                return status
+            else:
+                LOGGER.debug("(%s): %s", code, json_result)
+                return "UNKNOWN"
+        except HTTPError as e:
+            if "The connection to the validator was lost" in str(e.msg):
+                self._remove_unresponsive_validator(url)
+                LOGGER.warning("The validator at %s is no longer connected. "
+                               "Removing Validator.", url)
             return "UNKNOWN"
-
-        except FutureTimeoutError:
-            LOGGER.debug("The future timed out for %s.", batch_id)
+        except RemoteDisconnected as e:
+            self._remove_unresponsive_validator(url)
+            LOGGER.warning("The validator at %s is no longer connected. "
+                           "Removing Validator.", url)
+            return "UNKNOWN"
+        except URLError as e:
+            LOGGER.warning(
+                'Unable to connect to "%s": make sure URL is correct', url)
+            self._remove_unresponsive_validator(url)
             return "UNKNOWN"
