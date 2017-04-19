6c41a99c03c8e5833e3928a8ba89f4a9068f1749
Andrea Gunderson <andreax.gunderson@intel.com>
2017-04-19 11:59:51 -0500

    Add routed Batch and Block Response handling to the Responder.
    The Responder will keep track of received requests that it
    could not fulfill. When it receives a response to the
    broadcasted message, it will send the message back to the
    originally requester. This is done using a TimedCache
    to store the requests, and two new handlers.
    
    Signed-off-by: Andrea Gunderson <andreax.gunderson@intel.com>

--- validator/sawtooth_validator/journal/responder.py
+++ validator/sawtooth_validator/journal/responder.py
@@ -14,19 +14,28 @@
 # ------------------------------------------------------------------------------
 
 import logging
+import time
+from threading import RLock
 
 from sawtooth_validator.networking.dispatch import Handler
 from sawtooth_validator.networking.dispatch import HandlerResult
 from sawtooth_validator.networking.dispatch import HandlerStatus
+from sawtooth_validator.journal.timed_cache import TimedCache
 from sawtooth_validator.protobuf import network_pb2
 from sawtooth_validator.protobuf import validator_pb2
+from sawtooth_validator.protobuf import block_pb2
+from sawtooth_validator.protobuf import batch_pb2
 
 LOGGER = logging.getLogger(__name__)
 
 
 class Responder(object):
-    def __init__(self, completer):
+    def __init__(self, completer, cache_purge_frequency=30):
         self.completer = completer
+        self.pending_requests = TimedCache(cache_purge_frequency)
+        self._cache_purge_frequency = cache_purge_frequency
+        self._purge_time = time.time() + self._cache_purge_frequency
+        self._lock = RLock()
 
     def check_for_block(self, block_id):
         # Ask Completer
@@ -44,6 +53,31 @@ class Responder(object):
         batch = self.completer.get_batch_by_transaction(transaction_id)
         return batch
 
+    def add_request(self, requested_id, connection_id):
+        with self._lock:
+            if requested_id in self.pending_requests:
+                if connection_id not in self.pending_requests[requested_id]:
+                    self.pending_requests[requested_id] += [connection_id]
+
+            else:
+                self.pending_requests[requested_id] = [connection_id]
+
+    def get_request(self, requested_id):
+        with self._lock:
+            return self.pending_requests.get(requested_id)
+
+    def remove_request(self, requested_id):
+        with self._lock:
+            if requested_id in self.pending_requests:
+                del self.pending_requests[requested_id]
+
+    def purge_requests(self):
+        with self._lock:
+            if self._purge_time < time.time():
+                LOGGER.debug("Purge pending_requests of expired entries.")
+                self.pending_requests.purge_expired()
+                self._purge_time = time.time() + self._cache_purge_frequency
+
 
 class BlockResponderHandler(Handler):
     def __init__(self, responder, gossip):
@@ -57,7 +91,9 @@ class BlockResponderHandler(Handler):
         node_id = block_request_message.node_id
         block = self._responder.check_for_block(block_id)
         if block is None:
-            # No block found, broadcast orignal message to other peers
+            # No block found, broadcast original message to other peers
+            # and add to pending requests
+            self._responder.add_request(block_id, connection_id)
             self._gossip.broadcast(block_request_message,
                                    validator_pb2.Message.GOSSIP_BLOCK_REQUEST)
         else:
@@ -75,6 +111,34 @@ class BlockResponderHandler(Handler):
         return HandlerResult(status=HandlerStatus.PASS)
 
 
+class ResponderBlockResponseHandler(Handler):
+    def __init__(self, responder, gossip):
+        self._responder = responder
+        self._gossip = gossip
+
+    def handle(self, connection_id, message_content):
+        block_response = network_pb2.GossipBlockResponse()
+        block_response.ParseFromString(message_content)
+        block = block_pb2.Block()
+        block.ParseFromString(block_response.content)
+        open_request = self._responder.get_request(block.header_signature)
+
+        if open_request is None:
+            return HandlerResult(status=HandlerStatus.PASS)
+
+        for connection_id in open_request:
+            LOGGER.debug("Responding to block request: Send %s to %s",
+                         block.header_signature,
+                         connection_id)
+            self._gossip.send(validator_pb2.Message.GOSSIP_BLOCK_RESPONSE,
+                              message_content,
+                              connection_id)
+
+        self._responder.remove_request(block.header_signature)
+        self._responder.purge_requests()
+        return HandlerResult(status=HandlerStatus.PASS)
+
+
 class BatchByBatchIdResponderHandler(Handler):
     def __init__(self, responder, gossip):
         self._responder = responder
@@ -88,6 +152,10 @@ class BatchByBatchIdResponderHandler(Handler):
         node_id = batch_request_message.node_id
 
         if batch is None:
+            # No batch found, broadcast original message to other peers
+            # and add to pending requests
+            self._responder.add_request(batch_request_message.id,
+                                        connection_id)
             self._gossip.broadcast(
                 batch_request_message,
                 validator_pb2.Message.GOSSIP_BATCH_BY_BATCH_ID_REQUEST)
@@ -133,6 +201,8 @@ class BatchByTransactionIdResponderHandler(Handler):
             batch = None
 
         if batches == []:
+            for txn_id in batch_request_message.ids:
+                self._responder.add_request(txn_id, connection_id)
             self._gossip.broadcast(
                 batch_request_message,
                 validator_pb2.Message.
@@ -142,6 +212,8 @@ class BatchByTransactionIdResponderHandler(Handler):
             new_request = network_pb2.GossipBatchByTransactionIdRequest()
             new_request.ids.extend(unfound_txn_ids)
             new_request.node_id = batch_request_message.node_id
+            for txn_id in unfound_txn_ids:
+                self._responder.add_request(txn_id, connection_id)
             self._gossip.broadcast(
                 new_request,
                 validator_pb2.Message.
@@ -160,4 +232,41 @@ class BatchByTransactionIdResponderHandler(Handler):
                                   batch_response.SerializeToString(),
                                   connection_id)
 
+        return HandlerResult(status=HandlerStatus.PASS)
+
+
+class ResponderBatchResponseHandler(Handler):
+    def __init__(self, responder, gossip):
+        self._responder = responder
+        self._gossip = gossip
+
+    def handle(self, connection_id, message_content):
+        batch_response = network_pb2.GossipBatchResponse()
+        batch_response.ParseFromString(message_content)
+        batch = batch_pb2.Batch()
+        batch.ParseFromString(batch_response.content)
+        open_request = self._responder.get_request(batch.header_signature)
+
+        if open_request is None:
+            open_request = []
+
+        requests_to_remove = [batch.header_signature]
+        for txn in batch.transactions:
+            requests_by_txn = self._responder.get_request(txn.header_signature)
+            if requests_by_txn is not None:
+                open_request += requests_by_txn
+                requests_to_remove += [txn.header_signature]
+
+        for connection_id in open_request:
+            LOGGER.debug("Responding to batch requests: Send %s to %s",
+                         batch.header_signature,
+                         connection_id)
+            self._gossip.send(validator_pb2.Message.GOSSIP_BATCH_RESPONSE,
+                              message_content,
+                              connection_id)
+
+        for requested_id in requests_to_remove:
+            self._responder.remove_request(requested_id)
+
+        self._responder.purge_requests()
         return HandlerResult(status=HandlerStatus.PASS)
