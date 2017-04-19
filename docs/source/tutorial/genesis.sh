be6028728be6f34edbd4eeb1e9f6b9ae92645dcd
Shawn T. Amundson <amundson@bitwise.io>
2017-04-18 23:42:38 -0500

    Remove 0.7-era tutorial files
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- docs/source/tutorial/genesis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf /home/ubuntu/sawtooth
-mkdir /home/ubuntu/sawtooth
-mkdir /home/ubuntu/sawtooth/keys
-mkdir /home/ubuntu/sawtooth/data
-mkdir /home/ubuntu/sawtooth/logs
-/project/sawtooth-core/bin/sawtooth keygen base000 --key-dir /home/ubuntu/sawtooth/keys
-/project/sawtooth-core/bin/sawtooth admin poet1-genesis --keyfile /home/ubuntu/sawtooth/keys/base000.wif -F ledger.transaction.integer_key
-echo {\"InitialConnectivity\": 0} > /home/ubuntu/sawtooth/v0.json
