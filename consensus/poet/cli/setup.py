570d7a326de4728ba160761843a867e33a25be40
Ryan Beck-Buysse <rbuysse@bitwise.io>
2017-04-27 14:07:05 -0500

    Move version functions out of setup.py files
    Signed-off-by: Ryan Beck-Buysse <rbuysse@bitwise.io>

--- consensus/poet/cli/setup.py
+++ consensus/poet/cli/setup.py
@@ -16,58 +16,14 @@
 from __future__ import print_function
 
 import os
-import shutil
 import subprocess
-import sys
 
 from setuptools import setup, find_packages
 
 
-def bump_version(version):
-    (major, minor, patch) = version.split('.')
-    patch = str(int(patch) + 1)
-    return ".".join([major, minor, patch])
-
-
-def auto_version(default, strict):
-    output = subprocess.check_output(['git', 'describe', '--dirty'])
-    parts = output.decode('utf-8').strip().split('-', 1)
-    parts[0] = parts[0][1:]  # strip the leading 'v'
-    if len(parts) == 2:
-        parts[0] = bump_version(parts[0])
-    if default != parts[0]:
-        msg = "setup.py and (bumped?) git describe versions differ: {} != {}"\
-            .format(default, parts[0])
-        if strict:
-            print("ERROR: " + msg, file=sys.stderr)
-            sys.exit(1)
-        else:
-            print("WARNING: " + msg, file=sys.stderr)
-            print("WARNING: using setup.py version {}".format(
-                default), file=sys.stderr)
-            parts[0] = default
-
-    if len(parts) == 2:
-        return ".git".join([parts[0], parts[1].replace("-", ".")])
-    else:
-        return parts[0]
-
-
-def version(default):
-    if 'VERSION' in os.environ:
-        if os.environ['VERSION'] == 'AUTO_STRICT':
-            version = auto_version(default, strict=True)
-        elif os.environ['VERSION'] == 'AUTO':
-            version = auto_version(default, strict=False)
-        else:
-            version = os.environ['VERSION']
-    else:
-        version = default + ".dev1"
-    return version
-
-
 setup(name='sawtooth-poet-cli',
-      version=version('0.8.2'),
+      version=subprocess.check_output(
+          ['../../../bin/get_version']).decode('utf-8').strip(),
       description='Sawtooth Lake PoET CLI',
       author='Intel Corporation',
       url='https://github.com/hyperledger/sawtooth-core',
