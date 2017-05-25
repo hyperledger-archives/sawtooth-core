32cfea5c8e2186bfe678e60c69fc0abc1d22fb2b
Jamie Jason <jamie.jason@intel.com>
2017-05-25 15:00:20 -0700

    Move validator_reg_message_factory.py to tests directory
    Also fix up imports
    
    Signed-off-by: Jamie Jason <jamie.jason@intel.com>

--- consensus/poet/families/tests/test_tp_validator_registry.py
+++ consensus/poet/families/tests/test_tp_validator_registry.py
@@ -19,8 +19,7 @@ import hashlib
 
 from sawtooth_processor_test.transaction_processor_test_case \
     import TransactionProcessorTestCase
-from sawtooth_validator_registry.validator_reg_message_factory \
-    import ValidatorRegistryMessageFactory
+from validator_reg_message_factory import ValidatorRegistryMessageFactory
 
 from sawtooth_poet_common import sgx_structs
 from sawtooth_poet_common.protobuf.validator_registry_pb2 import \
