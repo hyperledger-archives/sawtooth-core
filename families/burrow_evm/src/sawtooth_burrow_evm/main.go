f8a671b4fe18ae276a95d3affe4a2fb0ed59d7d8
Adam Ludvik <ludvik@bitwise.io>
2017-05-26 15:27:24 -0500

    Use go-flags to parse args
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- families/burrow_evm/src/sawtooth_burrow_evm/main.go
+++ families/burrow_evm/src/sawtooth_burrow_evm/main.go
@@ -1,41 +1,59 @@
 package main
 
 import (
-	"flag"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"os"
 	burrow_evm "sawtooth_burrow_evm/handler"
 	"sawtooth_sdk/logging"
 	"sawtooth_sdk/processor"
 	"syscall"
 )
 
+type Opts struct {
+	Verbose []bool `short:"v" long:"verbose" description:"Increase verbosity"`
+}
+
 func main() {
-	v := flag.Bool("v", false, "Info level logging")
-	vv := flag.Bool("vv", false, "Debug level logging")
-	endpoint := "tcp://localhost:40000"
 
-	flag.Parse()
-	args := flag.Args()
-	if len(args) > 0 {
-		// Overwrite the default endpoint if specified
-		endpoint = args[0]
-	}
-
-	level := logging.WARN
-	if *v {
-		level = logging.INFO
-	}
-	if *vv {
-		level = logging.DEBUG
-	}
+	var opts Opts
 
 	logger := logging.Get()
-	logger.SetLevel(level)
+
+	parser := flags.NewParser(&opts, flags.Default)
+	remaining, err := parser.Parse()
+	if err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			logger.Errorf("Failed to parse args: %v", err)
+			os.Exit(2)
+		}
+	}
+	if len(remaining) > 1 {
+		fmt.Println("Must pass one or fewer endpoints")
+		os.Exit(2)
+	}
+
+	endpoint := "tcp://localhost:40000"
+	if len(remaining) == 1 {
+		endpoint = remaining[0]
+	}
+
+	switch len(opts.Verbose) {
+	case 2:
+		logger.SetLevel(logging.DEBUG)
+	case 1:
+		logger.SetLevel(logging.INFO)
+	default:
+		logger.SetLevel(logging.WARN)
+	}
 
 	handler := burrow_evm.NewBurrowEVMHandler()
 	processor := processor.NewTransactionProcessor(endpoint)
 	processor.AddHandler(handler)
 	processor.ShutdownOnSignal(syscall.SIGINT, syscall.SIGTERM)
-	err := processor.Start()
+	err = processor.Start()
 	if err != nil {
 		logger.Error("Processor stopped: ", err)
 	}
