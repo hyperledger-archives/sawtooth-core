b2702774c6c3b6d35d73c33c3e3d3b04d40f47e1
Ryan Beck-Buysse <rbuysse@bitwise.io>
2017-07-10 16:37:44 -0500

    Move version functions out of PoET SGX setup.py
    Signed-off-by: Ryan Beck-Buysse <rbuysse@bitwise.io>

--- consensus/poet/sgx/setup.py
+++ consensus/poet/sgx/setup.py
@@ -26,51 +26,6 @@ from shutil import copyfile
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 
-def bump_version(version):
-    (major, minor, patch) = version.split('.')
-    patch = str(int(patch) + 1)
-    return ".".join([major, minor, patch])
-
-
-def auto_version(default, strict):
-    output = subprocess.check_output(['git', 'describe', '--dirty'])
-    parts = output.strip().split('-', 1)
-    parts[0] = parts[0][1:]  # strip the leading 'v'
-    if len(parts) == 2:
-        parts[0] = bump_version(parts[0])
-    if default != parts[0]:
-        msg = "setup.py and (bumped?) git describe versions differ: {} != {}"\
-            .format(
-                default, parts[0])
-        if strict:
-            print('ERROR: {}'.format(msg), file=sys.stderr)
-            sys.exit(1)
-        else:
-            print('WARNING: {}'.format(msg), file=sys.stderr)
-            print('WARNING: using setup.py version {}'.format(
-                default),
-                file=sys.stderr)
-            parts[0] = default
-
-    if len(parts) == 2:
-        return "-git".join([parts[0], parts[1].replace("-", ".")])
-    else:
-        return parts[0]
-
-
-def version(default):
-    if 'VERSION' in os.environ:
-        if os.environ['VERSION'] == 'AUTO_STRICT':
-            version = auto_version(default, strict=True)
-        elif os.environ['VERSION'] == 'AUTO':
-            version = auto_version(default, strict=False)
-        else:
-            version = os.environ['VERSION']
-    else:
-        version = default + "-dev1"
-    return version
-
-
 if os.name == 'nt':
     platform_dir = 'windows'
     package_data = []
@@ -158,7 +113,8 @@ else:
     conf_dir = "/etc/sawtooth"
 
 setup(name='sawtooth-poet-sgx',
-      version=version('0.8.4'),
+      version=subprocess.check_output(
+          ['../../../bin/get_version']).decode('utf-8').strip(),
       description='Sawtooth Lake PoET SGX Enclave',
       author='Intel Corporation',
       url='http://www.intel.com',
