068bb8c18b72e159439725ef4ee76d11edb12fea
Boyd Johnson <boydx.johnson@intel.com>
2017-07-26 10:24:50 -0500

    Add license header to java signing and small cleanup
    Signed-off-by: Boyd Johnson <boydx.johnson@intel.com>

--- sdk/java/src/main/java/sawtooth/sdk/client/Signing.java
+++ sdk/java/src/main/java/sawtooth/sdk/client/Signing.java
@@ -1,4 +1,18 @@
-package sawtooth.sdk.client.sawtooth.sdk.client;
+/* Copyright 2017 Intel Corporation
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+------------------------------------------------------------------------------*/
+
+package sawtooth.sdk.client;
 
 import org.bitcoinj.core.DumpedPrivateKey;
 import org.bitcoinj.core.ECKey;
@@ -22,7 +36,7 @@ public class Signing {
   }
 
   public static String getPublicKey(ECKey privateKey) {
-    return "03" + privateKey.getPublicKeyAsHex();
+    return privateKey.getPublicKeyAsHex();
   }
 
   /**
