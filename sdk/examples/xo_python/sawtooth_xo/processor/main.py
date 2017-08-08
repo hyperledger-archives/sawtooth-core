d1c9578953f576b46d9d7dfa304bbb342a657c76
Adam Ludvik <ludvik@bitwise.io>
2017-08-08 12:41:50 -0500

    Support YAML format for logging config
    TOML does not support enough data types to support the SysLog handler type in
    python logging. YAML does.
    
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- sdk/examples/xo_python/sawtooth_xo/processor/main.py
+++ sdk/examples/xo_python/sawtooth_xo/processor/main.py
@@ -65,6 +65,11 @@ def main(args=None):
     try:
         processor = TransactionProcessor(url=opts.endpoint)
         log_config = get_log_config(filename="xo_log_config.toml")
+
+        # If no toml, try loading yaml
+        if log_config is None:
+            log_config = get_log_config(filename="xo_log_config.yaml")
+
         if log_config is not None:
             log_configuration(log_config=log_config)
         else:
