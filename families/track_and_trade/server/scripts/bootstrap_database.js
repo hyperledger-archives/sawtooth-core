1882c04fc86a806998a13986f95a2b8ba3466374
Zac Delventhal <delventhalz@gmail.com>
2017-08-24 22:26:13 -0500

    Add RethinkDB to Track and Trade client server
    Includes script to bootstrap a Track and Trade db with user
    and state tables, including a Hello World message, which is
    curlable from the tnt API.
    
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

---
+++ families/track_and_trade/server/scripts/bootstrap_database.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2017 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+'use strict'
+
+const r = require('rethinkdb')
+
+const HOST = process.env.DB_HOST || 'localhost'
+const PORT = process.env.DB_PORT || 28015
+const NAME = process.env.DB_NAME || 'tnt'
+
+r.connect({host: HOST, port: PORT})
+  .then(conn => {
+    console.log(`Creating "${NAME}" database...`)
+    r.dbList().contains(NAME).run(conn)
+      .then(dbExists => {
+        if (dbExists) throw new Error(`"${NAME}" already exists`)
+        return r.dbCreate(NAME).run(conn)
+      })
+      .then(res => {
+        console.log('Creating "users" table...')
+        return r.db(NAME).tableCreate('users').run(conn)
+      })
+      .then(res => {
+        console.log('Creating and populating "state" table...')
+        return r.db(NAME).tableCreate('state').run(conn)
+      })
+      .then(res => {
+        return r.db(NAME).table('state').insert({
+          name: 'message',
+          value: 'Hello Track and Trade!'
+        }).run(conn)
+      })
+      .then(res => {
+        console.log('Bootstrapping complete, closing connection.')
+        return conn.close()
+      })
+      .catch(err => {
+        console.log(`Unable to complete bootstrap of "${NAME}" db!`)
+        console.log(err)
+        return conn.close()
+      })
+  })
+  .catch(err => {
+    console.log(`Unable to connect to ${HOST}:${PORT}}!`)
+    console.log(err)
+  })
