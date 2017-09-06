def3cfeb474aa2b6ebe215e4b8160f6ef5d8ece8
Zac Delventhal <delventhalz@gmail.com>
2017-09-06 02:17:17 -0500

    Implement T&T state delta export handler
    Switches from PoC IntKey exports to T&T exports, properly
    decoding and inserting T&T state changes.
    
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- families/track_and_trade/server/db/blocks.js
+++ families/track_and_trade/server/db/blocks.js
@@ -19,6 +19,37 @@
 const r = require('rethinkdb')
 const db = require('./')
 
+const stateTables = [
+  'agents',
+  'records',
+  'recordTypes',
+  'properties',
+  'propertyPages',
+  'proposals'
+]
+
+const getForkedDocRemover = blockNum => tableName => {
+  return db.modifyTable(tableName, table => {
+    return table
+      .filter(r.row('startBlockNum').ge(blockNum))
+      .delete()
+      .do(() => table.filter(doc => doc('endBlockNum').ge(blockNum)))
+      .update({endBlockNum: Number.MAX_SAFE_INTEGER})
+  })
+}
+
+const resolveFork = block => {
+  const defork = getForkedDocRemover(block.blockNum)
+  return db.modifyTable('blocks', blocks => {
+    return blocks
+      .filter(r.row('blockNum').ge(block.blockNum))
+      .delete()
+      .do(() => blocks.insert(block))
+  })
+    .then(() => Promise.all(stateTables.map(tableName => defork(tableName))))
+    .then(() => block)
+}
+
 const insert = block => {
   return db.modifyTable('blocks', blocks => {
     return blocks
@@ -33,15 +64,7 @@ const insert = block => {
       if (!result.blockId || result.blockId === block.blockId) {
         return block
       }
-
-      // Otherwise, a fork has occurred, and must be resolved
-      return db.modifyTable('blocks', blocks => {
-        return blocks
-          .filter(r.row('blockNum').ge(block.blockNum))
-          .delete()
-          .do(() => blocks.insert(block))
-      })
-        .then(() => block)
+      return resolveFork(block)
     })
 }
 
