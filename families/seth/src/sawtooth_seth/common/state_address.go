6ebb7f0c81bf26e350408c6f616f33f647348091
Darian Plumb <dplumb@bitwise.io>
2017-09-13 15:02:07 -0500

    Implement BLOCKHEIGHT TIMESTAMP and BLOCKHASH opcodes
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- families/seth/src/sawtooth_seth/common/state_address.go
+++ families/seth/src/sawtooth_seth/common/state_address.go
@@ -50,6 +50,15 @@ func NewStateAddrFromString(s string) (sa StateAddr, err error) {
 	return NewStateAddrFromBytes(bytes)
 }
 
+func NewBlockInfoAddr(n int64) (StateAddr, error) {
+	s := fmt.Sprintf("%x", n)
+	bytes, err := hex.DecodeString(s)
+	if err != nil {
+		return "", fmt.Errorf("Unable to get block info address")
+	}
+	bytes = word256.LeftPadBytes(bytes, 31)
+	return StateAddr(BLOCK_INFO_NAMESPACE + hex.EncodeToString(bytes)), nil
+}
 
 func (sa StateAddr) String() string {
 	return string(sa)
