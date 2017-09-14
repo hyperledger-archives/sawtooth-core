be3d530c1a2671642a196994cb6da2341c72beab
Zac Delventhal <delventhalz@gmail.com>
2017-09-14 15:59:38 -0500

    Add basic Signup form to FishNet
    Takes user info and creates a user on the server. Does not yet
    generate a private key or submit a creat Agent transaction.
    
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

---
+++ families/track_and_trade/client/src/views/signup_form.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2017 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+'use strict'
+
+const m = require('mithril')
+const _ = require('lodash')
+
+const api = require('../services/api')
+const forms = require('../components/forms')
+
+/**
+ * The Form for authorizing an existing user.
+ */
+const SignupForm = {
+  view (vnode) {
+    const setter = forms.stateSetter(vnode.state)
+
+    return m('.signup-form.container', [
+      m('form', {
+        onsubmit: (e) => {
+          e.preventDefault()
+          api.post('users', _.assign(vnode.state, {publicKey: '' + Date.now()}))
+            .then(res => api.setAuth(res.authorization))
+        }
+      },
+      m('legend', 'Create Agent'),
+      forms.textInput(setter('name'), 'Name'),
+      forms.emailInput(setter('email'), 'Email'),
+      forms.textInput(setter('username'), 'Username'),
+      forms.passwordInput(setter('password'), 'Password'),
+      m('.container.text-center',
+        'Or you can ',
+        m('a[href="/login"]',
+          { oncreate: m.route.link },
+          'login an existing Agent')),
+      m('.form-group',
+        m('.row.justify-content-end.align-items-end',
+          m('col-2',
+            m('button.btn.btn-primary',
+              'Create Agent')))))
+    ])
+  }
+}
+
+module.exports = SignupForm
