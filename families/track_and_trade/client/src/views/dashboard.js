8b4c7d97ec1d2150bdd474483f32dfacbb1fabe9
Zac Delventhal <delventhalz@gmail.com>
2017-09-19 13:06:37 -0500

    Implement Dashboard page
    This commit removes the previous dashboard placeholder with a list of
    links to built pages, and replaces it with the dashboard as designed:
    A blurb of text detailing FishNet and Track and Trade.
    
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- families/track_and_trade/client/src/views/dashboard.js
+++ families/track_and_trade/client/src/views/dashboard.js
@@ -20,24 +20,46 @@ const m = require('mithril')
 
 const Dashboard = {
   view (vnode) {
-    return m('.container', [
-      m('h1', 'FishNet'),
-      m('.page-list',
-        m('h3', 'Pages'),
-        m('ul',
-          m('li',
-            m('a[href="/create"]',
-              { oncreate: m.route.link },
-              'Create Fish')),
-          m('li',
-            m('a[href="/signup"]',
-              { oncreate: m.route.link },
-              'Signup Agent')),
-          m('li',
-            m('a[href="/login"]',
-              { oncreate: m.route.link },
-              'Login Agent'))))
-    ])
+    return [
+      m('.header.text-center.mb-4',
+        m('h4', 'Welcome To'),
+        m('h1.mb-3', 'FishNet'),
+        m('h5',
+          m('em',
+            'Powered by ',
+            m('strong', 'Sawtooth: Track and Trade')))),
+      m('.blurb',
+        m('p',
+          m('em', 'Track and Trade'),
+          ' is a general purpose supply chain solution built using the ',
+          'power of ',
+          m('a[href="https://github.com/hyperledger/sawtooth-core"]',
+            { target: '_blank' },
+            "Hyperledger Sawtooth's"),
+          ' blockchain technology. It maintains a distributed ledger ',
+          'that tracks both asset provenance and a timestamped history ',
+          'detailing how an asset was stored, handled, and transported.'),
+        m('p',
+          m('em', 'FishNet'),
+          ' demonstrates this unique technology with an illustrative ',
+          'example: tracking the provenance of fish from catch to plate. ',
+          'One day an application like this could be used by restaurants, ',
+          'grocery stores, and their customers to ensure the fish they ',
+          'purchase is ethically sourced and properly transported.'),
+        m('p',
+          'To use ',
+          m('em', 'FishNet'),
+          ', create an account using the link in the navbar above. ',
+          'Once logged in, you will be able to add new fish assets to ',
+          'the blockchain and track them with data like temperature or ',
+          'location. You will be able to authorize other "agents" on the ',
+          'blockchain to track this data as well, or even transfer ',
+          'ownership or possession of the fish entirely. For the ',
+          'adventurous, these actions can also be accomplished directly ',
+          'with the REST API running on the ',
+          m('em', 'Track and Trade'),
+          ' server, perfect for automated IoT sensors.'))
+    ]
   }
 }
 
