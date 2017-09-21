3956883b6d0272bf67f44a35508e2c230965c832
Peter Schwarz <pschwarz@bitwise.io>
2017-09-21 13:55:53 -0500

    Properly copy sets as lists
    To avoid concurrent modifications of the `all_tasks` set, wrap the call in
    a list before iterating.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- sdk/python/sawtooth_sdk/messaging/stream.py
+++ sdk/python/sawtooth_sdk/messaging/stream.py
@@ -146,7 +146,8 @@ class _SendReceiveThread(Thread):
         LOGGER.debug("monitor socket received disconnect event")
         for future in self._futures.future_values():
             future.set_result(FutureError())
-        for task in asyncio.Task.all_tasks(self._event_loop).copy():
+        tasks = list(asyncio.Task.all_tasks(self._event_loop))
+        for task in tasks:
             task.cancel()
         self._event_loop.stop()
         self._send_queue = None
@@ -176,7 +177,8 @@ class _SendReceiveThread(Thread):
     def _cancel_tasks_yet_to_be_done(self):
         """Cancels all the tasks (pending coroutines and futures)
         """
-        for task in asyncio.Task.all_tasks(self._event_loop).copy():
+        tasks = list(asyncio.Task.all_tasks(self._event_loop))
+        for task in tasks:
             self._event_loop.call_soon_threadsafe(task.cancel)
         self._event_loop.call_soon_threadsafe(self._done_callback)
 
