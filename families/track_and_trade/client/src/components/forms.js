5b669f70c56821a03ef7e337be0e719e342ce897
Zac Delventhal <delventhalz@gmail.com>
2017-09-22 11:48:23 -0500

    Implement private key download for Agent Detail page
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- families/track_and_trade/client/src/components/forms.js
+++ families/track_and_trade/client/src/components/forms.js
@@ -81,6 +81,18 @@ const validator = (predicate, message, id = null) => e => {
   }
 }
 
+/**
+ * Triggers a download of a dynamically created text file
+ */
+const triggerDownload = (name, ...contents) => {
+  const file = new window.Blob(contents, {type: 'text/plain'})
+  const href = window.URL.createObjectURL(file)
+  const container = document.getElementById('download-container')
+  m.render(container, m('a#downloader', { href, download: name }))
+  document.getElementById('downloader').click()
+  m.mount(container, null)
+}
+
 module.exports = {
   group,
   field,
@@ -91,5 +103,6 @@ module.exports = {
   emailInput,
   clickIcon,
   stateSetter,
-  validator
+  validator,
+  triggerDownload
 }
