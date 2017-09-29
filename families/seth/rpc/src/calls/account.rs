450f1be792f859276ae0b83950c238925dac53f1
Adam Ludvik <ludvik@bitwise.io>
2017-09-28 19:49:59 -0500

    Abstract getting values from accounts
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- families/seth/rpc/src/calls/account.rs
+++ families/seth/rpc/src/calls/account.rs
@@ -26,6 +26,8 @@ use client::{
     bytes_to_hex_str,
 };
 
+use messages::seth::{EvmStateAccount};
+
 use sawtooth_sdk::messaging::stream::MessageSender;
 use error;
 use requests::{RequestHandler};
@@ -70,26 +72,9 @@ fn validate_storage_address(address: String) -> Result<String, Error> {
     }
 }
 
-pub fn get_balance<T>(params: Params, mut client: ValidatorClient<T>) -> Result<Value, Error> where T: MessageSender {
+pub fn get_balance<T>(params: Params, client: ValidatorClient<T>) -> Result<Value, Error> where T: MessageSender {
     info!("eth_getBalance");
-    let (address, block): (String, String) = match params.parse() {
-        Ok(t) => t,
-        Err(_) => {
-            return Err(Error::invalid_params("Takes [address: DATA(20), block: QUANTITY|TAG]"));
-        },
-    };
-
-    let key = validate_block_key(block)?;
-    let address = validate_account_address(address)?;
-
-    match client.get_account(address, key) {
-        Ok(Some(account)) => Ok(num_to_hex(&account.balance)),
-        Ok(None) => Ok(Value::Null),
-        Err(error) => {
-            error!("{}", error);
-            Err(Error::internal_error())
-        },
-    }
+    get_account(params, client, |account| num_to_hex(&account.balance))
 }
 
 pub fn get_storage_at<T>(params: Params, mut client: ValidatorClient<T>) -> Result<Value, Error> where T: MessageSender {
@@ -115,8 +100,12 @@ pub fn get_storage_at<T>(params: Params, mut client: ValidatorClient<T>) -> Resu
     }
 }
 
-pub fn get_code<T>(params: Params, mut client: ValidatorClient<T>) -> Result<Value, Error> where T: MessageSender {
+pub fn get_code<T>(params: Params, client: ValidatorClient<T>) -> Result<Value, Error> where T: MessageSender {
     info!("eth_getCode");
+    get_account(params, client, |account| hex_prefix(&bytes_to_hex_str(&account.code)))
+}
+
+fn get_account<T, F>(params: Params, mut client: ValidatorClient<T>, f: F) -> Result<Value, Error> where T: MessageSender, F: Fn(EvmStateAccount) -> Value {
     let (address, block): (String, String) = match params.parse() {
         Ok(t) => t,
         Err(_) => {
@@ -128,7 +117,7 @@ pub fn get_code<T>(params: Params, mut client: ValidatorClient<T>) -> Result<Val
     let address = validate_account_address(address)?;
 
     match client.get_account(address, key) {
-        Ok(Some(account)) => Ok(hex_prefix(&bytes_to_hex_str(&account.code))),
+        Ok(Some(account)) => Ok(f(account)),
         Ok(None) => Ok(Value::Null),
         Err(error) => {
             error!("{}", error);
