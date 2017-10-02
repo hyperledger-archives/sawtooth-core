33d43b34a4c789ffba560a73add9f0372d152d3b
Jamie Jason <jamie.jason@intel.com>
2017-10-02 10:44:50 -0700

    Add support for detecting disconnect/connect events for server/validator
    The message dispatcher is changed so that it will monitor the server socket for connect and disconnect events.  These events are then forwarded on to the transaction processor to deal with in the appropriate manner.
    
    Signed-off-by: Jamie Jason <jamie.jason@intel.com>

--- sdk/cxx/src/message_dispatcher.cpp
+++ sdk/cxx/src/message_dispatcher.cpp
@@ -27,6 +27,7 @@
 #include <zmqpp/socket.hpp>
 #include <zmqpp/socket_types.hpp>
 #include <zmqpp/socket_options.hpp>
+#include <zmq.h>
 
 #include "proto/processor.pb.h"
 
@@ -37,6 +38,9 @@ namespace sawtooth {
 
 const std::string MessageDispatcher::DISPATCH_THREAD_ENDPOINT(
     "inproc://dispatch_thread");
+const std::string MessageDispatcher::SERVER_MONITOR_ENDPOINT(
+    "inproc://server_monitor");
+
 const std::string MessageDispatcher::THREAD_READY_MESSAGE("READY");
 const std::string MessageDispatcher::THREAD_EXIT_MESSAGE("EXIT");
 
@@ -48,34 +52,14 @@ MessageDispatcher::MessageDispatcher():
         server_socket(this->context_, zmqpp::socket_type::dealer),
         message_socket(this->context_, zmqpp::socket_type::dealer),
         processing_request_socket(this->context_, zmqpp::socket_type::dealer),
-        dispatch_thread_socket(this->context_, zmqpp::socket_type::pair)
-        { }
-
-// Connect to the validator component endpoint socket and
-// create thread to service the socket, an internal socket is
-// also created to receive messages to be forwarded to the validator.
-void MessageDispatcher::Connect(const std::string& connection_string) {
-    LOG4CXX_INFO(logger, "Connecting to " << connection_string);
-    bool connected = false;
-    do {
-        try {
-            this->server_socket.connect(connection_string.c_str());
-            connected = true;
-        } catch(std::exception& e) {
-            LOG4CXX_DEBUG(logger, "Connection to validator failed, "
-                << "waiting to retry: " << e.what());
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        }
-    } while (!connected);
-
+        dispatch_thread_socket(this->context_, zmqpp::socket_type::pair),
+        server_is_connected(false) {
     this->message_socket.bind("inproc://send_queue");
     this->processing_request_socket.bind("inproc://request_queue");
+    this->dispatch_thread_socket.bind(MessageDispatcher::DISPATCH_THREAD_ENDPOINT);
 
-    // Bind to the socket used to communicate with the dispatch thread,
-    // start the thread to process incoming messages, and then wait for the
+    // Start the thread to process incoming messages, and then wait for the
     // thread to indicate it is ready
-    this->dispatch_thread_socket.bind(
-        MessageDispatcher::DISPATCH_THREAD_ENDPOINT);
     this->dispatch_thread = std::thread(
         [this] {
             this->DispatchThread();
@@ -86,15 +70,42 @@ void MessageDispatcher::Connect(const std::string& connection_string) {
     } while(throw_away_message != MessageDispatcher::THREAD_READY_MESSAGE);
 }
 
-void MessageDispatcher::Close() {
-    // signal the dispatch thread to stop and join.
-    this->dispatch_thread_socket.send(MessageDispatcher::THREAD_EXIT_MESSAGE);
+MessageDispatcher::~MessageDispatcher() {
+    // Signal the dispatch thread to stop and join.
+    this->dispatch_thread_socket.send(
+        MessageDispatcher::THREAD_EXIT_MESSAGE, true);
     this->dispatch_thread.join();
+}
 
-    this->dispatch_thread_socket.close();
-    this->server_socket.close();
-    this->message_socket.close();
-    this->processing_request_socket.close();
+// Connect to the validator component endpoint socket
+void MessageDispatcher::Connect(const std::string& connection_string) {
+    LOG4CXX_INFO(logger, "Connecting to " << connection_string);
+
+    // It is worth noting that the connect message returning successfully does
+    // not actually indicate that the connection has completed.  Per the ZMQ
+    // documentation:
+    //
+    // "...for most transports and socket types the connection is not performed
+    // immediately but as needed by ØMQ. Thus a successful call to zmq_connect()
+    // does not mean that the connection was or could actually be established."
+    //
+    // Therefore, we will perform the connection and we will simply monitor, in
+    // the dispatch thread, the server socket to detect the connected event
+    // and from that we can let the transaction processor know.
+    try {
+        this->server_socket.connect(connection_string.c_str());
+    } catch(std::exception& e) {
+        LOG4CXX_ERROR(logger, "Connection to validator failed: " << e.what());
+        throw;
+    }
+}
+
+void MessageDispatcher::Close() {
+    LOG4CXX_INFO(logger, "Disconnect server socket");
+    std::string last_endpoint =
+        this->server_socket.get<std::string>(
+            zmqpp::socket_option::last_endpoint);
+    this->server_socket.disconnect(last_endpoint);
 }
 
 void MessageDispatcher::ReceiveMessage() {
@@ -109,10 +120,7 @@ void MessageDispatcher::ReceiveMessage() {
 
     switch (msg_proto->message_type()) {
         case Message_MessageType_TP_PROCESS_REQUEST: {
-            zmqpp::message out_zmsg;
-            const std::string& content = msg_proto->content();
-            out_zmsg.add(content.data(), content.length());
-            this->processing_request_socket.send(out_zmsg);
+            this->processing_request_socket.send(zmsg);
             break;
         }
         case Message_MessageType_TP_PING: {
@@ -167,6 +175,22 @@ void MessageDispatcher::SendMessage() {
     this->server_socket.send(message);
 }
 
+uint16_t MessageDispatcher::GetServerSocketEvent(
+    zmqpp::socket& server_monitor_socket
+    ) {
+    // First part of the message contains event number/type and value.  We only
+    // care about the event type.
+    zmqpp::message event;
+    server_monitor_socket.receive(event);
+
+    // Note - don't be tempted to use the get() method or the stream extraction
+    // operator (>>) as they _seem_ (I say seem because I haven't looked at the
+    // source code) to assume that the 16-bit integer has been transmitted in
+    // big endian, but it appears that the server monitor socket data is simply
+    // in processor native format.
+    return *reinterpret_cast<const uint16_t *>(event.raw_data());
+}
+
 MessageStreamPtr MessageDispatcher::CreateStream() {
     return std::shared_ptr<MessageStream>(new MessageStream(&this->context_,
         this->message_futures, this->mutex));
@@ -185,6 +209,32 @@ void MessageDispatcher::DispatchThread() {
     dispatch_socket.send(std::string(MessageDispatcher::THREAD_READY_MESSAGE));
     socket_poller.add(dispatch_socket);
 
+    // Start monitoring the server socket for the connect and disconnect events.
+    //
+    // In reality, what we would really like is to monitor at this time for
+    // connect and then when the state changes to connected, monitor solely for
+    // the disconnect event and vice versa.  That way we poll in the most
+    // efficient way as once we are in the disconnected state, the monitor will
+    // keep giving is disconnect events if we are monitoring them.  However,
+    // the problem seems to be that once we start monitoring, we are not allowed
+    // to change what we are monitoring for.  Would love if someone could solve
+    // this problem.
+    if (zmq_socket_monitor(
+            this->server_socket,
+            MessageDispatcher::SERVER_MONITOR_ENDPOINT.c_str(),
+            ZMQ_EVENT_CONNECTED | ZMQ_EVENT_DISCONNECTED)) {
+        LOG4CXX_ERROR(
+            logger,
+            "Error trying to monitor server socket: "
+                << zmq_errno());
+        throw std::runtime_error("Failed to monitor server socket events");
+    }
+
+    // Create a socket for monitoring the server socket and add it to the poller
+    zmqpp::socket server_monitor_socket(this->context_, zmqpp::socket_type::pair);
+    server_monitor_socket.connect(MessageDispatcher::SERVER_MONITOR_ENDPOINT);
+    socket_poller.add(server_monitor_socket);
+
     bool threadShouldExit = false;
 
     while (!threadShouldExit) {
@@ -195,14 +245,51 @@ void MessageDispatcher::DispatchThread() {
         if (socket_poller.has_input(this->message_socket)) {
             this->SendMessage();
         }
+        if (socket_poller.has_input(server_monitor_socket)) {
+            // Get the next event for the server socket
+            uint16_t event = this->GetServerSocketEvent(server_monitor_socket);
+
+            // If we are not connected to the server and the event was a
+            // connected event or we are connected and the event was a
+            // disconnected event, we need to let the transaction processor
+            // know so that it can perform the proper action.
+            if ((server_is_connected && (ZMQ_EVENT_DISCONNECTED & event)) ||
+                (!server_is_connected && (ZMQ_EVENT_CONNECTED & event))) {
+                // Set the server connection state appropriately
+                server_is_connected = ZMQ_EVENT_CONNECTED == event;
+                LOG4CXX_INFO(
+                    logger,
+                    "Server connection state changed to: "
+                        << (server_is_connected ? "CONNECTED" : "DISCONNECTED"));
+
+                // Create a new message for the transaction processor and set
+                // its type to an appropriate event type value and queue up the
+                // message in the transaction processors queue.
+                Message msg;
+                msg.set_message_type(
+                    ZMQ_EVENT_CONNECTED == event ?
+                        SERVER_CONNECT_EVENT : SERVER_DISCONNECT_EVENT);
+
+                std::string msg_data;
+                msg.SerializeToString(&msg_data);
+
+                zmqpp::message zmsg;
+                zmsg.add(msg_data.data(), msg_data.length());
+                this->processing_request_socket.send(zmsg);
+            }
+        }
         if (socket_poller.has_input(dispatch_socket)) {
             std::string message;
             dispatch_socket.receive(message);
-            threadShouldExit = message == MessageDispatcher::THREAD_EXIT_MESSAGE;
+            threadShouldExit = (message == MessageDispatcher::THREAD_EXIT_MESSAGE);
         }
     }
 
-    dispatch_socket.close();
+    // Stop monitoring the server socket.  The documentation doesn't mention
+    // this, but it appears that passing NULL for the endpoint causes the
+    // monitoring to stop.
+    server_monitor_socket.disconnect(MessageDispatcher::SERVER_MONITOR_ENDPOINT);
+    zmq_socket_monitor(this->server_socket, nullptr, ZMQ_EVENT_ALL);
 }
 
 }  // namespace sawtooth
