2449b5ba79d565b0cc3bdcbff3462b630473e678
Shawn T. Amundson <amundson@bitwise.io>
2017-10-28 09:49:01 -0500

    Add initial Intkey TP written in Rust
    This is an initial skeleton of the intkey TP, which basiccally
    functions but does not yet pass the Intkey Transaction Family
    Specification -- that will be resovled in subsequent commits.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

---
+++ sdk/examples/intkey_rust/src/main.rs
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017 Bitwise IO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------------
+ */
+
+extern crate cbor;
+#[macro_use]
+extern crate clap;
+extern crate crypto;
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+extern crate sawtooth_sdk;
+extern crate rustc_serialize;
+
+mod handler;
+
+use log::LogLevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Config, Root};
+use log4rs::encode::pattern::PatternEncoder;
+
+use sawtooth_sdk::processor::TransactionProcessor;
+
+use handler::IntkeyTransactionHandler;
+
+fn main() {
+    let matches = clap_app!(intkey =>
+        (version: crate_version!())
+        (about: "Intkey Transaction Processor (Rust)")
+        (@arg endpoint: --endpoint +takes_value
+         "connection endpoint for validator")
+        (@arg verbose: -v --verbose +multiple
+         "increase output verbosity")).get_matches();
+
+    let endpoint = matches.value_of("endpoint").unwrap_or("tcp://localhost:4004");
+
+    let console_log_level;
+    match matches.occurrences_of("verbose") {
+        0 => console_log_level = LogLevelFilter::Warn,
+        1 => console_log_level = LogLevelFilter::Info,
+        2 => console_log_level = LogLevelFilter::Debug,
+        3 | _ =>console_log_level = LogLevelFilter::Trace
+    }
+
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{h({l:5.5})} | {({M}:{L}):20.20} | {m}{n}")))
+        .build();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(console_log_level))
+        .unwrap();
+
+    log4rs::init_config(config).unwrap();
+
+    let handler = IntkeyTransactionHandler::new();
+    let mut processor = TransactionProcessor::new(endpoint);
+
+    info!("Console logging level: {}", console_log_level);
+
+    processor.add_handler(&handler);
+    processor.start();
+}
