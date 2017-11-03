e74e1df693900ce6f7b69067b573c88ff597c13f
Andrea Gunderson <agunde@bitwise.io>
2017-11-03 13:54:48 -0500

    Put a try except around network.send during authorization
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- validator/sawtooth_validator/networking/interconnect.py
+++ validator/sawtooth_validator/networking/interconnect.py
@@ -784,12 +784,13 @@ class Interconnect(object):
         the validator to be authorized by the incoming connection.
         """
         connect_message = ConnectionRequest(endpoint=self._public_endpoint)
-        self.send(validator_pb2.Message.NETWORK_CONNECT,
-                  connect_message.SerializeToString(),
-                  connection_id,
-                  callback=partial(self._inbound_connection_request_callback,
-                                   connection_id=connection_id
-                                   ))
+        self._safe_send(validator_pb2.Message.NETWORK_CONNECT,
+                        connect_message.SerializeToString(),
+                        connection_id,
+                        callback=partial(
+                            self._inbound_connection_request_callback,
+                            connection_id=connection_id
+                            ))
 
     def _connect_callback(self, request, result,
                           connection=None):
@@ -854,7 +855,7 @@ class Interconnect(object):
             auth_trust_request = AuthorizationTrustRequest(
                 roles=[RoleType.Value("NETWORK")],
                 public_key=self._public_key)
-            self.send(
+            self._safe_send(
                 validator_pb2.Message.AUTHORIZATION_TRUST_REQUEST,
                 auth_trust_request.SerializeToString(),
                 connection_id
@@ -862,7 +863,7 @@ class Interconnect(object):
 
         if auth_type["challenge"]:
             auth_challenge_request = AuthorizationChallengeRequest()
-            self.send(
+            self._safe_send(
                 validator_pb2.Message.AUTHORIZATION_CHALLENGE_REQUEST,
                 auth_challenge_request.SerializeToString(),
                 connection_id,
@@ -912,7 +913,7 @@ class Interconnect(object):
             signature=signature,
             roles=[RoleType.Value("NETWORK")])
 
-        self.send(
+        self._safe_send(
             validator_pb2.Message.AUTHORIZATION_CHALLENGE_SUBMIT,
             auth_challenge_submit.SerializeToString(),
             connection_id
@@ -1134,6 +1135,17 @@ class Interconnect(object):
                 ConnectionType.OUTBOUND_CONNECTION:
             return True
         return False
+
+    def _safe_send(self, message_type, message, connection_id, callback=None):
+        try:
+            self.send(
+                message_type,
+                message,
+                connection_id,
+                callback=callback
+                )
+        except ValueError:
+            LOGGER.debug("Connection disconnected: %s", connection_id)
 
 
 class OutboundConnection(object):
