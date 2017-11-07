778e86cefdc45ab2eadb6bba43347f756643113d
Adam Ludvik <ludvik@bitwise.io>
2017-11-07 15:34:09 -0600

    Pass consensus in as a method argument
    Having consensus as an instance variable requires that a new block validator be
    created for every block, since consensus can be different between blocks.
    Passing it in as an argument moves the BlockValidator closer to being a
    persistent object.
    
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- validator/sawtooth_validator/journal/chain.py
+++ validator/sawtooth_validator/journal/chain.py
@@ -234,7 +234,6 @@ class ChainController(object):
                     state_view)
 
             validator = BlockValidator(
-                consensus_module=consensus_module,
                 new_block=blkw,
                 block_cache=self._block_cache,
                 state_view_factory=self._state_view_factory,
@@ -246,7 +245,8 @@ class ChainController(object):
                 permission_verifier=self._permission_verifier,
                 metrics_registry=self._metrics_registry)
             self._blocks_processing[blkw.block.header_signature] = validator
-            self._thread_pool.submit(validator.run, self.on_block_validated)
+            self._thread_pool.submit(
+                validator.run, consensus_module, self.on_block_validated)
 
     def on_block_validated(self, commit_new_block, result):
         """Message back from the block validator, that the validation is
@@ -456,7 +456,6 @@ class ChainController(object):
                         state_view)
 
                 validator = BlockValidator(
-                    consensus_module=consensus_module,
                     new_block=block,
                     block_cache=self._block_cache,
                     state_view_factory=self._state_view_factory,
@@ -468,7 +467,7 @@ class ChainController(object):
                     permission_verifier=self._permission_verifier,
                     metrics_registry=self._metrics_registry)
 
-                valid = validator.validate_block(block)
+                valid = validator.validate_block(block, consensus_module)
                 if valid:
                     if chain_id is None:
                         self._chain_id_manager.save_block_chain_id(
