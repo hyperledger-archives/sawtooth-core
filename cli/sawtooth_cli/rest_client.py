b8436a4917ee7a4d685acd4498dfcfba44004cbf
Nick Drozd <drozd@bitwise.io>
2017-12-16 17:39:04 -0600

    Catch missing schema exception in rest client
    Signed-off-by: Nick Drozd <drozd@bitwise.io>

--- cli/sawtooth_cli/rest_client.py
+++ cli/sawtooth_cli/rest_client.py
@@ -190,6 +190,8 @@ class RestClient(object):
             return (e.response.status_code, e.response.reason)
         except RemoteDisconnected as e:
             raise CliException(e)
+        except requests.exceptions.MissingSchema as e:
+            raise CliException(e)
         except requests.exceptions.ConnectionError as e:
             raise CliException(
                 ('Unable to connect to "{}": '
