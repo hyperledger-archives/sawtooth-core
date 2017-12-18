80ad1113f8f23b9842cf96174567338801eb64b3
Peter Schwarz <pschwarz@bitwise.io>
2017-12-18 13:08:43 -0600

    Add rejection of batches via back-pressure
    Add handlers for rejecting batches recieved via client submission These
    handlers are placed at the beginning of the handler chain for each of
    the tasks, in order for the process to be ended as early as possible.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- integration/sawtooth_integration/tests/intkey_client.py
+++ integration/sawtooth_integration/tests/intkey_client.py
@@ -17,6 +17,7 @@ import time
 from urllib.parse import urlparse, parse_qs
 
 from sawtooth_cli.rest_client import RestClient
+from sawtooth_cli.exceptions import CliException
 from sawtooth_cli.exceptions import RestClientException
 from sawtooth_intkey.intkey_message_factory import IntkeyMessageFactory
 
@@ -31,9 +32,20 @@ class IntkeyClient(RestClient):
     def send_txns(self, txns):
         batch = self.factory.create_batch(txns)
 
-        response = self.send_batches(batch)
-        id_query = urlparse(response['link']).query
-        return parse_qs(id_query)['id'][0]
+        attempts = 0
+        response = None
+        while True:
+            try:
+                response = self.send_batches(batch)
+                id_query = urlparse(response['link']).query
+                return parse_qs(id_query)['id'][0]
+            except CliException:
+                if attempts < 8:
+                    print('responding to back-pressure, retrying...')
+                    attempts += 1
+                    time.sleep(0.2 * (2 ** attempts))
+                else:
+                    raise
 
     def recent_block_signatures(self, tolerance):
         return self.list_block_signatures()[:tolerance]
