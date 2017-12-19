343c582ca8da8be9ecd5bdc74b5f6a3c9e9d0d63
Yevgeniy (Eugene) Yarmosh <yevgeniy.y.yarmosh@intel.com>
2017-12-19 00:53:11 +0000

    Intkey CXX example rework: Utilizing new CXX SDK API Example how to use protobuf for TX input data (instead of CBOR).    CBOR/PROTOBUF selection is done via a command line option Added command line processing Added log4xxx level control via commnd line (for example, not for SDK itself)
    Signed-off-by: Yevgeniy (Eugene) Yarmosh <yevgeniy.y.yarmosh@intel.com>

--- sdk/examples/intkey_cxx/int_key_transaction_processor.cpp
+++ sdk/examples/intkey_cxx/int_key_transaction_processor.cpp
@@ -12,17 +12,36 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 ------------------------------------------------------------------------------*/
+
+#include <ctype.h>
+#include <string.h>
+
 #include <iostream>
 #include <string>
 
 #include "log4cxx/logger.h"
 #include "log4cxx/basicconfigurator.h"
+#include "log4cxx/level.h"
 
-#include "sawtooth/address_mapper.h"
-#include "sawtooth/transaction_processor.h"
-#include "sawtooth/exceptions.h"
+#include "sawtooth_sdk.h"
+#include "exceptions.h"
+
+#include "../proto/cxx/intkey_tp.pb.h"
+
+// #include "sawtooth/address_mapper.h"
+// #include "sawtooth/transaction_processor.h"
+// #include "sawtooth/exceptions.h"
 
 #include "json.hpp"
+
+
+#define URL_PREFIX       "tcp://"
+#define URL_PREFIX_LEN   6
+#define URL_DEFAULT      "tcp://127.0.0.1:4004"
+
+
+
+using google::protobuf::MessageLite;
 
 using namespace log4cxx;
 
@@ -55,11 +74,13 @@ class IntKeyApplicator:  public sawtooth::TransactionApplicator {
     IntKeyApplicator(sawtooth::TransactionUPtr txn,
         sawtooth::GlobalStateUPtr state) :
         TransactionApplicator(std::move(txn), std::move(state)),
-            address_mapper(INTKEY_NAMESPACE) { }
+            address_mapper
+	    (sawtooth::AddressMapperIF::Create(INTKEY_NAMESPACE)) 
+	{ };
 
-    void Apply() {
-        LOG4CXX_DEBUG(logger, "IntKeyApplicator::Apply");
 
+    void CborToParams(std::string& verb, std::string& name, int& value)
+    {
         const std::string& raw_data = this->txn->payload();
         std::vector<uint8_t> data_vector = ToVector(raw_data);
         json intkey_cmd = json::from_cbor(data_vector);
@@ -68,19 +89,23 @@ class IntKeyApplicator:  public sawtooth::TransactionApplicator {
             throw sawtooth::InvalidTransaction(
                 "CBOR Object as the encoded command");
         }
+
         auto verb_it = intkey_cmd.find("Verb");
         if (verb_it == intkey_cmd.end()) {
             throw sawtooth::InvalidTransaction(
                 "Verb is required");
         }
-        std::string verb = *verb_it;
+
+        verb = *verb_it;
 
         auto name_it = intkey_cmd.find("Name");
         if (name_it == intkey_cmd.end()) {
             throw sawtooth::InvalidTransaction(
                 "Name is required");
         }
-        std::string name = *name_it;
+
+        name = *name_it;
+
         if (name.length() == 0 || name.length() > 20) {
             throw sawtooth::InvalidTransaction(
                 "Name is invalid, name must be between" \
@@ -92,7 +117,42 @@ class IntKeyApplicator:  public sawtooth::TransactionApplicator {
             throw sawtooth::InvalidTransaction(
                 "Value is required");
         }
-        int value = (*value_it).get<int>();
+
+        value = (*value_it).get<int>();
+    };
+
+
+    void ProtobufToParams(std::string& verb, std::string& name, int& value)
+    {
+        const std::string& rawData = this->txn->payload();
+        IntkeyTp intkeyProto;
+
+        if (!intkeyProto.ParseFromString(rawData))
+        {
+           throw sawtooth::InvalidTransaction(
+                "PROTOBUF payload is inavalid");
+        }
+
+        verb = intkeyProto.verb();
+        name = intkeyProto.name();
+        value = intkeyProto.value();
+    }
+
+    void Apply() {
+        LOG4CXX_DEBUG(logger, "IntKeyApplicator::Apply");
+        std::string verb;
+        std::string name;
+        int value;
+
+        if (bProtobuf)
+        {
+            ProtobufToParams(verb, name, value);
+        }
+        else
+        {
+            CborToParams(verb, name, value);
+        }
+
 
         if (verb == "set") {
             this->DoSet(name, value);
@@ -108,12 +168,15 @@ class IntKeyApplicator:  public sawtooth::TransactionApplicator {
         }
     }
 
+
+    static bool bProtobuf;
+
  private:
-    sawtooth::AddressMapper address_mapper;
+    sawtooth::AddressMapperUPtr address_mapper;
 
  private:
     std::string MakeAddress(const std::string& name) {
-        return this->address_mapper.MakeAddress(name);
+        return this->address_mapper->MakeAddress(name);
     }
 
     // Handle an IntKey 'set' verb action. This sets a IntKey value to
@@ -226,53 +289,240 @@ class IntKeyApplicator:  public sawtooth::TransactionApplicator {
 
 // Defines the IntKey Handler to register with the transaction processor
 // sets the versions and types of transactions that can be handled.
-class IntKeyHandler: public sawtooth::TransactionHandler {
- public:
-    std::string transaction_family_name() const {
+class IntKeyHandler: public sawtooth::TransactionHandler
+{
+public:
+    IntKeyHandler()
+    {
+        sawtooth::AddressMapperUPtr
+            addr(sawtooth::AddressMapperIF::Create(INTKEY_NAMESPACE));
+
+        namespacePrefix = addr->GetNamespacePrefix();
+    }
+
+    std::string transaction_family_name() const
+    {
         return std::string("intkey");
     }
+
     std::list<std::string> versions() const {
         return {"1.0"};
     }
-    std::list<std::string> namespaces() const {
-        sawtooth::AddressMapper addr(INTKEY_NAMESPACE);
-        return { addr.GetNamespacePrefix() };
+
+    std::list<std::string> namespaces() const
+    {
+        // sawtooth::AddressMapperUPtr addr(
+		// sawtooth::AddressMapperIF::Create(INTKEY_NAMESPACE));
+        // return { addr->GetNamespacePrefix() };
+        return { namespacePrefix };
     }
+
     sawtooth::TransactionApplicatorUPtr GetApplicator(
         sawtooth::TransactionUPtr txn,
-        sawtooth::GlobalStateUPtr state) {
+        sawtooth::GlobalStateUPtr state)
+    {
         return sawtooth::TransactionApplicatorUPtr(
             new IntKeyApplicator(std::move(txn), std::move(state)));
     }
+private:
+    std::string namespacePrefix;
 };
 
+bool IntKeyApplicator::bProtobuf = false;
+
+void Usage(bool bExit = false, int exitCode = 1)
+{
+	std::cout << "Usage" << std::endl;
+	std::cout << "intkey_cxx [options] [connet_string]" << std::endl;
+	std::cout << "  -h, --help - print this message" << std::endl;
+
+	std::cout <<
+	"  -v, -vv, -vvv - detailed logging output, more letters v more details"
+	<< std::endl;
+
+	std::cout <<
+	"  -p, --protobuf - use protobuf format for TX data instead if CBOR"
+	<< std::endl;
+
+	std::cout <<
+	"  connect_string - connect string to validator in format tcp://host:port"
+	<< std::endl;
+
+	if (bExit)
+	{
+		exit(exitCode);
+	}
+}
+
+
+bool TestConnectString(const char* str)
+{
+	const char* ptr = str;
+
+    if (strncmp(str, URL_PREFIX, URL_PREFIX_LEN))
+    {
+		return false;
+    }
+
+    ptr = str + URL_PREFIX_LEN;
+
+	if (!isdigit(*ptr))
+	{
+		if (*ptr == ':' || (ptr = strchr(ptr, ':')) == NULL)
+		{
+			return false;
+		}
+		ptr++;
+	}
+	else
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			if (!isdigit(*ptr))
+			{
+				return false;
+			}
+
+			ptr++;
+			if (isdigit(*ptr))
+			{
+				ptr++;
+				if (isdigit(*ptr))
+				{
+					ptr++;
+				}
+			}
+
+			if (i < 3)
+			{
+				if (*ptr != '.')
+				{
+					return false;
+				}
+			}
+			else
+			{
+				if (*ptr != ':')
+				{
+                    return false;
+				}
+			}
+			ptr++;
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (!isdigit(*ptr))
+		{
+			if (i == 0)
+			{
+				return false;
+			}
+			break;
+		}
+		ptr++;
+	}
+
+	if (*ptr != 0)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void ParseArgs(int argc, char** argv, bool& bProto, std::string& connectStr)
+{
+    bool bLogLevelSet = false;
+
+	for (int i = 1; i < argc; i++)
+	{
+		const char* arg = argv[i];
+		if (!strcmp(arg, "-h") || !strcmp(arg, "--help"))
+		{
+			Usage(true, 0);
+		}
+		else
+		if (!strcmp(arg, "-v"))
+		{
+			// logLevel = Level::WARN_INT;
+			logger->setLevel(Level::getWarn());
+			bLogLevelSet = true;
+		}
+		else
+		if (!strcmp(arg, "-vv"))
+		{
+			// logLevel = Level::INFO_INT;
+			logger->setLevel(Level::getInfo());
+			bLogLevelSet = true;
+		}
+		else
+		if (!strcmp(arg, "-vvv"))
+		{
+			// logLevel = Level::ALL_INT; //DEBUG_INT, TRACE_INT;
+			logger->setLevel(Level::getAll());
+			bLogLevelSet = true;
+		}
+		else
+		if (!strcmp(arg, "-p") || !strcmp(arg, "--protobuf"))
+		{
+			bProto = true;
+		}
+		else
+		if (i != (argc - 1))
+		{
+			std::cout << "Invalid command line argument:" << arg << std::endl;
+			Usage(true);
+		}
+		else
+		if (!TestConnectString(arg))
+		{
+			std::cout << "Connect string is not in format host:port - "
+			<< arg << std::endl;
+			Usage(true);
+		}
+		else
+		{
+			connectStr = arg;
+		}
+	}
+
+	if (!bLogLevelSet)
+	{
+	    logger->setLevel(Level::getError());
+	}
+}
 
 int main(int argc, char** argv) {
     try {
-        std::string connectString = "tcp://127.0.0.1:4004";
+        std::string connectString = URL_DEFAULT;
 
-        // Check if there is a command-line parameter.
-        // currently we expect the command line arguement to be
-        // a connection string.
-        if (argc > 2) {
-            throw std::runtime_error("");
-        } else if (argc == 2) {
-            connectString = argv[1];
-        }
+        ParseArgs(argc, argv, IntKeyApplicator::bProtobuf, connectString);
+
+        if (IntKeyApplicator::bProtobuf)
+            std::cout << "Using PROTOBUF transaction format" << std::endl;
+        else
+            std::cout << "Using CBOR transaction format" << std::endl;
 
         // Set up a simple configuration that logs on the console.
         BasicConfigurator::configure();
 
         // Create a transaction processor and register our
         // handlers with it.
-        sawtooth::TransactionProcessor processor(connectString);
-        sawtooth::TransactionHandlerUPtr transaction_handler(
+		sawtooth::TxProcessorIF* p =
+		sawtooth::TxProcessorIF::Create(connectString);        
+		sawtooth::TransactionProcessorUPtr processor(p);
+        
+		sawtooth::TransactionHandlerUPtr transaction_handler(
             new IntKeyHandler());
-        processor.RegisterHandler(
+
+		processor->RegisterHandler(
             std::move(transaction_handler));
 
-        LOG4CXX_DEBUG(logger, "Run");
-        processor.Run();
+        LOG4CXX_DEBUG(logger, "\nRun");
+
+        processor->Run();
 
         return 0;
     } catch(std::exception& e) {
