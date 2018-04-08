a744629d9854ffc19ef491a76d20746890f3bea1
yarasan <yarasan@users.noreply.github.com>
2018-04-08 23:06:28 +0300

    Fix file permission error while starting up validator
    Fixed error handling in case of permission errors while writing to debug/error files of the validator component. This fix refers to the issue, STL-505.
    
    Signed-off-by: yarasan <yarasan@users.noreply.github.com>

--- validator/sawtooth_validator/server/cli.py
+++ validator/sawtooth_validator/server/cli.py
@@ -122,6 +122,13 @@ def main(args):
         LOGGER.error(str(local_config_err))
         sys.exit(1)
 
+    try:
+        log_configuration(log_dir=path_config.log_dir,
+                          name="validator")
+    except LocalConfigurationError as local_config_err:
+        LOGGER.error(str(local_config_err))
+        sys.exit(1)
+
     # Process initial initialization errors, delaying the sys.exit(1) until
     # all errors have been reported to the user (via LOGGER.error()).  This
     # is intended to provide enough information to the user so they can correct
@@ -132,8 +139,6 @@ def main(args):
             key_dir=path_config.key_dir,
             key_name='validator')
     except LocalConfigurationError as e:
-        log_configuration(log_dir=path_config.log_dir,
-                          name="validator")
         LOGGER.error(str(e))
         init_errors = True
 
