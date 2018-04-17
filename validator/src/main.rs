c268efeefcf9b27758c642e5a79145a2ceb4d88a
Nick Drozd <drozd@bitwise.io>
2018-04-17 11:30:18 -0500

    Rustify CLI arg parsing
    CLI arguments are parsed in Rust and then passed into Python as a PyDict.
    
    Signed-off-by: Nick Drozd <drozd@bitwise.io>

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1,22 +1,25 @@
+extern crate clap;
 extern crate cpython;
 
-use cpython::{PyString, Python};
+mod server;
+
+use server::cli;
+use cpython::Python;
 
 fn main() {
     let gil = Python::acquire_gil();
     let py = &mut gil.python();
 
-    let mut args: Vec<PyString> = std::env::args()
-        .skip(1)
-        .map(|s| PyString::new(*py, &s))
-        .collect();
+    let args = cli::parse_args();
 
-    args.insert(0, PyString::new(*py, env!("CARGO_PKG_NAME")));
+    let pydict = cli::wrap_in_pydict(py, &args)
+        .map_err(|err| err.print(*py))
+        .unwrap();
 
     let cli = py.import("sawtooth_validator.server.cli")
         .map_err(|err| err.print(*py))
         .unwrap();
-    cli.call(*py, "main", (args,), None)
+    cli.call(*py, "main", (pydict,), None)
         .map_err(|err| err.print(*py))
         .unwrap();
 }
