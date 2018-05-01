f57efad819955a3f746d0156e12557d2f46bbf82
Peter Schwarz <pschwarz@bitwise.io>
2018-05-01 10:57:10 -0500

    Only regenerate proto sources on change
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- validator/build.rs
+++ validator/build.rs
@@ -22,40 +22,11 @@ use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
 use std::path::Path;
+use std::time::{Duration, UNIX_EPOCH};
 
 const PROTO_FILES_DIR: &str = "../protos";
 const PROTOBUF_TARGET_DIR: &str = "src/proto";
-
-fn main() {
-    // Generate protobuf files
-    let proto_src_files = glob_simple(&format!("{}/*.proto", PROTO_FILES_DIR));
-    println!("{:?}", proto_src_files);
-
-    fs::create_dir_all(PROTOBUF_TARGET_DIR).unwrap();
-
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: PROTOBUF_TARGET_DIR,
-        input: &proto_src_files
-            .iter()
-            .map(|&(_, ref a)| a)
-            .map(|s| s.as_ref())
-            .collect::<Vec<&str>>(),
-        includes: &["src", PROTO_FILES_DIR],
-    }).expect("unable to run protoc");
-
-    let mod_file_name = format!("{}/mod.rs", PROTOBUF_TARGET_DIR);
-    let mod_file_path = Path::new(&mod_file_name);
-    let mut file = match fs::File::create(&mod_file_path) {
-        Err(err) => panic!(
-            "Unable to create file {}: {}",
-            mod_file_name,
-            err.description()
-        ),
-        Ok(file) => file,
-    };
-
-    let content = format!(
-        r#"
+const GENERATED_SOURCE_HEADER: &str = r#"
 /*
  * THIS IS A GENERATED FILE: DO NOT MODIFY
  *
@@ -63,39 +34,133 @@ fn main() {
  * buffers messages.
  */
 
-{}"#,
-        proto_src_files
-            .iter()
-            .map(|&(ref module, _)| format!("pub mod {};", module))
-            .collect::<Vec<_>>()
-            .join("\n")
+"#;
+
+#[derive(Debug, Clone)]
+struct ProtoFile {
+    module_name: String,
+    file_path: String,
+    last_modified: Duration,
+}
+
+fn main() {
+    // Generate protobuf files
+    let proto_src_files = glob_simple(&format!("{}/*.proto", PROTO_FILES_DIR));
+    let last_build_time = read_last_build_time();
+
+    let latest_change = proto_src_files.iter().fold(
+        Duration::from_secs(0),
+        |max, ref proto_file| {
+            if proto_file.last_modified > max {
+                proto_file.last_modified
+            } else {
+                max
+            }
+        },
     );
-    match file.write_all(content.as_bytes()) {
-        Err(err) => panic!(
-            "Unable to write to {}: {}",
-            mod_file_name,
-            err.description()
-        ),
-        Ok(_) => println!("generated {}", mod_file_name),
+
+    if latest_change > last_build_time {
+        println!("{:?}", proto_src_files);
+        fs::create_dir_all(PROTOBUF_TARGET_DIR).unwrap();
+        protoc_rust::run(protoc_rust::Args {
+            out_dir: PROTOBUF_TARGET_DIR,
+            input: &proto_src_files
+                .iter()
+                .map(|proto_file| proto_file.file_path.as_ref())
+                .collect::<Vec<&str>>(),
+            includes: &["src", PROTO_FILES_DIR],
+        }).expect("unable to run protoc");
+
+        let mod_file_name = format!("{}/mod.rs", PROTOBUF_TARGET_DIR);
+        let mod_file_path = Path::new(&mod_file_name);
+        let mut file = match fs::File::create(&mod_file_path) {
+            Err(err) => panic!(
+                "Unable to create file {}: {}",
+                mod_file_name,
+                err.description()
+            ),
+            Ok(file) => file,
+        };
+
+        let content = format!(
+            "{}\n{}",
+            GENERATED_SOURCE_HEADER,
+            proto_src_files
+                .iter()
+                .map(|proto_file| format!("pub mod {};", proto_file.module_name))
+                .collect::<Vec<_>>()
+                .join("\n")
+        );
+        match file.write_all(content.as_bytes()) {
+            Err(err) => panic!(
+                "Unable to write to {}: {}",
+                mod_file_name,
+                err.description()
+            ),
+            Ok(_) => println!("generated {}", mod_file_name),
+        }
+    } else {
+        println!(
+            "No proto files changed; latest modification: {}, last build: {}",
+            latest_change.as_secs(),
+            last_build_time.as_secs()
+        );
     }
 }
 
-fn glob_simple(pattern: &str) -> Vec<(String, String)> {
+fn glob_simple(pattern: &str) -> Vec<ProtoFile> {
     glob::glob(pattern)
         .expect("glob")
-        .map(|g| {
-            let path = g.expect("item");
-
-            let module_name = path.as_path()
-                .file_stem()
-                .expect("file stem")
-                .to_str()
-                .expect("utf-8")
-                .to_owned();
-
-            let file_path = path.as_path().to_str().expect("utf-8").to_owned();
-
-            (module_name, file_path)
-        })
+        .map(|g| protofile_info(g.expect("item").as_path()))
         .collect()
+}
+
+fn protofile_info(path: &Path) -> ProtoFile {
+    let module_name = path.file_stem()
+        .expect("file stem")
+        .to_str()
+        .expect("utf-8")
+        .to_owned();
+
+    let file_path = path.to_str().expect("utf-8").to_owned();
+
+    let file = fs::File::open(path).expect("Unable to open file");
+    let last_modified = file.metadata()
+        .expect("file to have metadata")
+        .modified()
+        .map(|sys_time| {
+            sys_time
+                .duration_since(UNIX_EPOCH)
+                .expect("after UNIX_EPOCH")
+        })
+        .expect("file to have modified time");
+
+    ProtoFile {
+        module_name,
+        file_path,
+        last_modified,
+    }
+}
+
+fn read_last_build_time() -> Duration {
+    let mod_file_name = format!("{}/mod.rs", PROTOBUF_TARGET_DIR);
+    match fs::File::open(Path::new(&mod_file_name)) {
+        Err(err) => {
+            println!(
+                "unable to open {}: {}; defaulting to 0",
+                mod_file_name,
+                err.description()
+            );
+            Duration::new(0, 0)
+        }
+        Ok(file) => file.metadata()
+            .expect("file to have metadata")
+            .modified()
+            .map(|sys_time| {
+                sys_time
+                    .duration_since(UNIX_EPOCH)
+                    .expect("after UNIX_EPOCH")
+            })
+            .expect("file to have modified time"),
+    }
 }
