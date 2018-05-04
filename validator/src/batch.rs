5a76944b6d0202d453ac8ec33abd6723d9231d39
Boyd Johnson <bjohnson@bitwise.io>
2018-05-04 16:17:49 -0500

    Add Rust coretypes of Block, Batch, Transaction
    These Rust structs allow for manipulation of Blocks, Batches, and Transactions
    without having to deserialize fields that are bytes.
    
    Later either a From or a TryFrom can be implemented to convert from
    proto::{Block, Batch, Transaction}.
    
    Signed-off-by: Boyd Johnson <bjohnson@bitwise.io>

--- validator/src/batch.rs
+++ validator/src/batch.rs
@@ -14,23 +14,14 @@
  * limitations under the License.
  * ------------------------------------------------------------------------------
  */
-extern crate cbor;
-extern crate cpython;
-extern crate crypto;
-extern crate libc;
-extern crate lmdb_zero;
-extern crate protobuf;
 
-#[macro_use]
-extern crate log;
-#[cfg(test)]
-extern crate rand;
+use transaction::Transaction;
 
-// exported modules
-pub mod database;
-pub mod proto;
-pub mod state;
-
-pub mod batch;
-pub mod block;
-pub mod transaction;
+#[derive(Clone, Debug, PartialEq)]
+pub struct Batch {
+    pub header_signature: String,
+    pub transactions: Vec<Transaction>,
+    pub signer_public_key: String,
+    pub transaction_ids: Vec<String>,
+    pub trace: bool,
+}
