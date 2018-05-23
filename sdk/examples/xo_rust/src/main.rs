11a968e662663b85fc85e49e4336fa06fa795bb5
Bridger Herman <scooteeroo@gmail.com>
2018-05-23 14:15:28 -0500

    Add Rust XO transaction processor
    Add a processor for the XO transaction type, written in the Rust
    language.
    
    Signed-off-by: Bridger Herman <bridger@bitwise.io>

---
+++ sdk/examples/xo_rust/src/main.rs
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2018 Bitwise IO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------------
+ */
+
+#[macro_use]
+extern crate clap;
+extern crate crypto;
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+extern crate sawtooth_sdk;
+
+mod handler;
+
+use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Config, Root};
+use log4rs::encode::pattern::PatternEncoder;
+
+use sawtooth_sdk::processor::TransactionProcessor;
+
+use handler::handler::XoTransactionHandler;
+
+use std::process;
+
+fn main() {
+    let matches = clap_app!(xo =>
+        (version: crate_version!())
+        (about: "XO Transaction Processor (Rust)")
+        (@arg connect: -C --connect +takes_value
+         "connection endpoint for validator")
+        (@arg verbose: -v --verbose +multiple
+         "increase output verbosity"))
+        .get_matches();
+
+    let endpoint = matches
+        .value_of("connect")
+        .unwrap_or("tcp://localhost:4004");
+
+    let console_log_level;
+    match matches.occurrences_of("verbose") {
+        0 => console_log_level = LevelFilter::Warn,
+        1 => console_log_level = LevelFilter::Info,
+        2 => console_log_level = LevelFilter::Debug,
+        3 | _ => console_log_level = LevelFilter::Trace,
+    }
+
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "{h({l:5.5})} | {({M}:{L}):20.20} | {m}{n}",
+        )))
+        .build();
+
+    let config = match Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(console_log_level))
+    {
+        Ok(x) => x,
+        Err(e) => {
+            for err in e.errors().iter() {
+                error!("Configuration error: {}", err.to_string());
+            }
+            process::exit(1);
+        }
+    };
+
+    match log4rs::init_config(config) {
+        Ok(_) => (),
+        Err(e) => {
+            error!("Configuration error: {}", e.to_string());
+            process::exit(1);
+        }
+    }
+
+    let handler = XoTransactionHandler::new();
+    let mut processor = TransactionProcessor::new(endpoint);
+
+    info!("Console logging level: {}", console_log_level);
+
+    processor.add_handler(&handler);
+    processor.start();
+}
