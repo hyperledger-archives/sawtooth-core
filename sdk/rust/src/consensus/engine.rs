b786bbfd3510735bb770212c45310a65e52a106f
Nick Drozd <drozd@bitwise.io>
2018-05-23 12:41:07 -0500

    Derive PartialOrd for consensus BlockId
    Devmode needs this for fork resolution (it falls back to block id
    ordering if the two forks have the same height).
    
    Signed-off-by: Nick Drozd <drozd@bitwise.io>

--- sdk/rust/src/consensus/engine.rs
+++ sdk/rust/src/consensus/engine.rs
@@ -32,7 +32,7 @@ pub enum Update {
     BlockCommit(BlockId),
 }
 
-#[derive(Clone, Default, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Default, Debug, Eq, Hash, PartialEq, PartialOrd)]
 pub struct BlockId(Vec<u8>);
 impl Deref for BlockId {
     type Target = Vec<u8>;
