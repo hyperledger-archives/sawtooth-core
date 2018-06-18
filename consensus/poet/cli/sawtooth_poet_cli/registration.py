03ebb3508c6245074719c6d9cb0860083e83aa2f
Nick Drozd <drozd@bitwise.io>
2018-06-18 10:05:33 -0500

    Modify PoET import paths for engine use
    The engine can't import from the validator, so change its protobuf
    imports to use the SDK. Importing in try/except blocks maintains
    backwards compatibility.
    
    Signed-off-by: Nick Drozd <drozd@bitwise.io>

--- consensus/poet/cli/sawtooth_poet_cli/registration.py
+++ consensus/poet/cli/sawtooth_poet_cli/registration.py
@@ -34,9 +34,15 @@ from sawtooth_signing import ParseError
 from sawtooth_signing.secp256k1 import Secp256k1PrivateKey
 
 from sawtooth_validator.journal.block_wrapper import NULL_BLOCK_IDENTIFIER
-import sawtooth_validator.protobuf.transaction_pb2 as txn_pb
-import sawtooth_validator.protobuf.batch_pb2 as batch_pb
 from sawtooth_validator.state.settings_view import SettingsView
+
+try:
+    import sawtooth_validator.protobuf.transaction_pb2 as txn_pb
+    import sawtooth_validator.protobuf.batch_pb2 as batch_pb
+except ImportError:
+    import sawtooth_sdk.protobuf.transaction_pb2 as txn_pb
+    import sawtooth_sdk.protobuf.batch_pb2 as batch_pb
+
 
 VR_NAMESPACE = sha256('validator_registry'.encode()).hexdigest()[0:6]
 VALIDATOR_MAP_ADDRESS = \
