6fd3b523fa05365cf5977d9c0143ed8494cf579d
Peter Schwarz <pschwarz@bitwise.io>
2018-07-25 15:39:10 -0500

    Add sig thread pool workers parameter
    Add a parameter to Validator for the signature threadpool worker count.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- validator/sawtooth_validator/server/core.py
+++ validator/sawtooth_validator/server/core.py
@@ -92,7 +92,8 @@ class Validator:
                  network_private_key=None,
                  roles=None,
                  component_thread_pool_workers=10,
-                 network_thread_pool_workers=10):
+                 network_thread_pool_workers=10,
+                 signature_thread_pool_workers=3):
         """Constructs a validator instance.
 
         Args:
@@ -121,6 +122,8 @@ class Validator:
                 component thread pool; defaults to 10.
             network_thread_pool_workers (int): number of workers in the network
                 thread pool; defaults to 10.
+            signature_thread_pool_workers (int): number of workers in the
+                signature thread pool; defaults to 3.
         """
         # -- Setup Global State Database and Factory -- #
         global_state_db_filename = os.path.join(
@@ -168,7 +171,7 @@ class Validator:
             max_workers=5,
             name='Client')
         sig_pool = InstrumentedThreadPoolExecutor(
-            max_workers=3,
+            max_workers=signature_thread_pool_workers,
             name='Signature')
 
         # -- Setup Dispatchers -- #
