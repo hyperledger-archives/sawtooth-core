8851f6d83482dcc19f637a1ade1e58ad37562e90
Bridger Herman <bridger@bitwise.io>
2018-08-21 11:43:54 -0500

    Change output location of Rust SDK protobuf generation
    Signed-off-by: Bridger Herman <bridger@bitwise.io>

--- sdk/rust/build.rs
+++ sdk/rust/build.rs
@@ -19,7 +19,11 @@ extern crate cc;
 extern crate glob;
 extern crate protoc_rust;
 
+use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 
 use protoc_rust::Customize;
 
@@ -38,10 +42,33 @@ fn main() {
     let proto_src_files = glob_simple("../../protos/*.proto");
     println!("{:?}", proto_src_files);
 
-    fs::create_dir_all("src/messages").unwrap();
+    let out_dir = env::var("OUT_DIR").expect("No OUT_DIR env variable");
+    let dest_path = Path::new(&out_dir).join("messages");
+    fs::create_dir_all(&dest_path).expect("Unable to create proto destination directory");
+
+    let mod_file_content = proto_src_files
+        .iter()
+        .map(|proto_file| {
+            let proto_path = Path::new(proto_file);
+            format!(
+                "pub mod {};",
+                proto_path
+                    .file_stem()
+                    .expect("Unable to extract stem")
+                    .to_str()
+                    .expect("Unable to extract filename")
+            )
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    let mut mod_file = File::create(dest_path.join("mod.rs")).unwrap();
+    mod_file
+        .write_all(mod_file_content.as_bytes())
+        .expect("Unable to write mod file");
 
     protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/messages",
+        out_dir: &dest_path.to_str().expect("Invalid proto destination path"),
         input: &proto_src_files
             .iter()
             .map(|a| a.as_ref())
