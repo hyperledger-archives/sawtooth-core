6d7bc6be36c2cc1e48165f9c65ad9ab70029067f
Boyd Johnson <bjohnson@bitwise.io>
2018-08-27 08:11:37 -0500

    Add BlockValidationProcessor to run each individual validation
    This design allows for loose coupling between the individual validations
    and the code that orders the validations, which is easier to test and to
    add more validations.
    
    Signed-off-by: Boyd Johnson <bjohnson@bitwise.io>

--- validator/src/journal/block_validator.rs
+++ validator/src/journal/block_validator.rs
@@ -19,13 +19,15 @@ use cpython;
 use cpython::{FromPyObject, ObjectProtocol, PyObject, Python};
 
 use block::Block;
-use journal::block_wrapper::BlockStatus;
+use journal::{block_manager::BlockManager, block_store::BlockStore, block_wrapper::BlockStatus};
 use scheduler::TxnExecutionResult;
 use std::sync::mpsc::Sender;
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum ValidationError {
     BlockValidationFailure(String),
+    BlockValidationError(String),
+    BlockStoreUpdated,
 }
 
 pub trait BlockValidator: Sync + Send + Clone {
@@ -68,5 +70,122 @@ impl<'source> FromPyObject<'source> for BlockValidationResult {
             num_transactions,
             status,
         })
+    }
+}
+
+/// A generic block validation. Returns a ValidationError::BlockValidationFailure on
+/// validation failure. It is a dependent validation if it can return
+/// ValidationError::BlockStoreUpdated and is an independent validation otherwise
+trait BlockValidation {
+    type ReturnValue;
+
+    fn validate_block(
+        &self,
+        block: &Block,
+        previous_state_root: Option<&String>,
+    ) -> Result<Self::ReturnValue, ValidationError>;
+}
+
+/// A check that determines if the Dependent checks are honored. If this check
+/// returns false, the dependent checks are honored.
+trait BlockStoreUpdatedCheck {
+    fn check_chain_head_updated(
+        &self,
+        expected_chain_head_id: Option<&String>,
+    ) -> Result<bool, ValidationError>;
+}
+
+struct BlockValidationProcessor<
+    BS: BlockStore,
+    SBV: BlockValidation<ReturnValue = BlockValidationResult>,
+    C: BlockStoreUpdatedCheck,
+> {
+    block_store: BS,
+    block_manager: BlockManager,
+    dependent_validations: Vec<Box<BlockValidation<ReturnValue = ()>>>,
+    independent_validations: Vec<Box<BlockValidation<ReturnValue = ()>>>,
+    state_validation: SBV,
+    check: C,
+}
+
+impl<
+        BS: BlockStore,
+        SBV: BlockValidation<ReturnValue = BlockValidationResult>,
+        C: BlockStoreUpdatedCheck,
+    > BlockValidationProcessor<BS, SBV, C>
+{
+    fn new(
+        block_store: BS,
+        block_manager: BlockManager,
+        dependent_validations: Vec<Box<BlockValidation<ReturnValue = ()>>>,
+        independent_validations: Vec<Box<BlockValidation<ReturnValue = ()>>>,
+        state_validation: SBV,
+        check: C,
+    ) -> Self {
+        BlockValidationProcessor {
+            block_store,
+            block_manager,
+            dependent_validations,
+            independent_validations,
+            state_validation,
+            check,
+        }
+    }
+
+    fn validate_block(&self, block: &Block) -> Result<BlockValidationResult, ValidationError> {
+        let previous_blocks_state_hash = self
+            .block_manager
+            .get(&[&block.previous_block_id])
+            .next()
+            .unwrap_or(None)
+            .map(|b| b.state_root_hash.clone());
+
+        let checks = 'outer: loop {
+            let chain_head_option = self
+                .block_store
+                .iter()
+                .map_err(|err| {
+                    ValidationError::BlockValidationError(format!(
+                        "There was an error reading from the BlockStore: {:?}",
+                        err
+                    ))
+                })?
+                .next()
+                .map(|b| b.header_signature.clone());
+            let mut dependent_checks = vec![];
+            for validation in &self.dependent_validations {
+                match validation.validate_block(&block, previous_blocks_state_hash.as_ref()) {
+                    Ok(()) => (),
+                    Err(ValidationError::BlockStoreUpdated) => {
+                        warn!(
+                            "Blockstore updated during validation of block {}, retrying checks",
+                            &block.header_signature
+                        );
+                        continue 'outer;
+                    }
+                    Err(err) => dependent_checks.push(Err(err)),
+                }
+            }
+
+            if !self
+                .check
+                .check_chain_head_updated(chain_head_option.as_ref())?
+            {
+                break dependent_checks;
+            }
+        };
+        for check in checks {
+            check?;
+        }
+
+        for validation in &self.independent_validations {
+            match validation.validate_block(&block, previous_blocks_state_hash.as_ref()) {
+                Ok(()) => (),
+                Err(err) => return Err(err),
+            }
+        }
+
+        self.state_validation
+            .validate_block(&block, previous_blocks_state_hash.as_ref())
     }
 }
