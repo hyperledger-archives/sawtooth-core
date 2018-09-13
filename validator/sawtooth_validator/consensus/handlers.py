92677bcf0c86a537ebb943b8edb38f49094a551d
Adam Ludvik <ludvik@bitwise.io>
2018-09-13 10:08:11 -0700

    Define Publisher exceptions in publisher file
    These are all publisher specific exceptions raised by the publisher, so they
    should be defined with the publisher. Defining them with the handlers more
    tightly couples the handlers and the publisher.
    
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- validator/sawtooth_validator/consensus/handlers.py
+++ validator/sawtooth_validator/consensus/handlers.py
@@ -28,6 +28,9 @@ from sawtooth_validator.networking.dispatch import HandlerResult
 from sawtooth_validator.networking.dispatch import HandlerStatus
 
 from sawtooth_validator.journal.block_wrapper import BlockStatus
+from sawtooth_validator.journal.publisher import BlockEmpty
+from sawtooth_validator.journal.publisher import BlockInProgress
+from sawtooth_validator.journal.publisher import BlockNotInitialized
 
 from sawtooth_validator.protobuf.block_pb2 import BlockHeader
 from sawtooth_validator.protobuf.consensus_pb2 import ConsensusSettingsEntry
@@ -35,18 +38,6 @@ from sawtooth_validator.protobuf.consensus_pb2 import ConsensusStateEntry
 
 
 LOGGER = logging.getLogger(__name__)
-
-
-class BlockEmpty(Exception):
-    """There are no batches in the block."""
-
-
-class BlockInProgress(Exception):
-    """There is already a block in progress."""
-
-
-class BlockNotInitialized(Exception):
-    """There is no block in progress to finalize."""
 
 
 class ConsensusServiceHandler(Handler):
--- validator/sawtooth_validator/journal/publisher.py
+++ validator/sawtooth_validator/journal/publisher.py
@@ -25,9 +25,6 @@ from sawtooth_validator import ffi
 from sawtooth_validator.ffi import PY_LIBRARY, LIBRARY
 from sawtooth_validator.ffi import OwnedPointer
 
-from sawtooth_validator.consensus.handlers import BlockEmpty
-from sawtooth_validator.consensus.handlers import BlockInProgress
-from sawtooth_validator.consensus.handlers import BlockNotInitialized
 from sawtooth_validator.journal.block_wrapper import BlockWrapper
 
 LOGGER = logging.getLogger(__name__)
@@ -116,6 +113,18 @@ class BlockPublisherErrorCode(IntEnum):
     BlockInProgress = 0x03
     BlockNotInitialized = 0x04
     BlockEmpty = 0x05
+
+
+class BlockEmpty(Exception):
+    """There are no batches in the block."""
+
+
+class BlockInProgress(Exception):
+    """There is already a block in progress."""
+
+
+class BlockNotInitialized(Exception):
+    """There is no block in progress to finalize."""
 
 
 class BlockPublisher(OwnedPointer):
