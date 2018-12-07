21ffd72c3a55a35be8eebd3218f62da752bc8647
Andrea Gunderson <agunde@bitwise.io>
2018-12-07 09:29:27 -0600

    Fix rust sdk for stable rust_fmt
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- sdk/rust/src/consensus/zmq_driver.rs
+++ sdk/rust/src/consensus/zmq_driver.rs
@@ -168,7 +168,8 @@ pub fn register(
             Message_MessageType::CONSENSUS_REGISTER_REQUEST,
             &generate_correlation_id(),
             &request,
-        )?.get_timeout(timeout)?;
+        )?
+        .get_timeout(timeout)?;
 
     let ret: Result<StartupState, Error>;
 
@@ -209,7 +210,8 @@ pub fn register(
                                 Message_MessageType::CONSENSUS_REGISTER_REQUEST,
                                 &generate_correlation_id(),
                                 &request,
-                            )?.get_timeout(timeout)?;
+                            )?
+                            .get_timeout(timeout)?;
 
                         continue;
                     }
