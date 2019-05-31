6915638a1754362606efa5a4d6a34b6507569e13
Peter Schwarz <pschwarz@bitwise.io>
2019-05-31 16:25:41 -0500

    Add IdentityError
    Add IdentityError enum for errors that can occur when processing
    permissions.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- validator/src/permissions/mod.rs
+++ validator/src/permissions/mod.rs
@@ -14,6 +14,7 @@
  * limitations under the License.
  * ------------------------------------------------------------------------------
  */
+pub mod error;
 
 pub enum Permission {
     PermitKey(String),
