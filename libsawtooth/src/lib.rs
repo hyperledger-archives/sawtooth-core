27ae1e5194b828b03ddad06703e75c2f55097b06
Logan Seeley <seeley@bitwise.io>
2019-11-21 11:47:44 -0600

    Add `log` dependency
    Signed-off-by: Logan Seeley <seeley@bitwise.io>

--- libsawtooth/src/lib.rs
+++ libsawtooth/src/lib.rs
@@ -12,5 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[macro_use]
+extern crate log;
+
 #[cfg(feature = "stores")]
 pub mod store;
