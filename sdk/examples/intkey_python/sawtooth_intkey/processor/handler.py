fcfc6d8bc67a5d5a3b67b9e60a5f6b8d2c4c8cd5
S m, Aruna <aruna.mohan@walmartlabs.com>
2020-04-10 01:55:45 +0530

    Fix pylint R1721 in SDK
    Unnecessary use of a comprehension
    
    Signed-off-by: S m, Aruna <aruna.mohan@walmartlabs.com>

--- sdk/examples/intkey_python/sawtooth_intkey/processor/handler.py
+++ sdk/examples/intkey_python/sawtooth_intkey/processor/handler.py
@@ -170,7 +170,7 @@ def _do_set(name, value, state):
                 n=name,
                 v=state[name]))
 
-    updated = {k: v for k, v in state.items()}
+    updated = dict(state.items())
     updated[name] = value
 
     return updated
@@ -192,7 +192,7 @@ def _do_inc(name, value, state):
             'Verb is "inc", but result would be greater than {}'.format(
                 MAX_VALUE))
 
-    updated = {k: v for k, v in state.items()}
+    updated = dict(state.items())
     updated[name] = incd
 
     return updated
@@ -214,7 +214,7 @@ def _do_dec(name, value, state):
             'Verb is "dec", but result would be less than {}'.format(
                 MIN_VALUE))
 
-    updated = {k: v for k, v in state.items()}
+    updated = dict(state.items())
     updated[name] = decd
 
     return updated
