f117e7eef6c07ebb905b7f013285e16553ecafd6
Logan Seeley <seeley@bitwise.io>
2020-06-17 14:37:13 -0500

    Initialize journal module for libsawtooth
    The `journal` module is behind the experimental `validator-internals`
    feature.
    
    Signed-off-by: Logan Seeley <seeley@bitwise.io>

--- libsawtooth/src/journal/mod.rs
+++ libsawtooth/src/journal/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 Cargill Incorporated
+// Copyright 2020 Cargill Incorporated
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,12 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#[cfg(feature = "lmdb-store")]
-#[macro_use]
-extern crate log;
-
-#[cfg(feature = "validator-internals")]
-pub mod journal;
-#[cfg(feature = "stores")]
-pub mod store;
