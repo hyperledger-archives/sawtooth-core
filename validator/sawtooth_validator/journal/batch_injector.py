de66e8053fcabfc8cc9c344708ff71930302aa42
Carlos Alejandro Gutierrez Sandoval <carlos.gutierrez@gluwa.com>
2021-08-24 09:22:55 -0500

    gluwa injector

--- validator/sawtooth_validator/journal/batch_injector.py
+++ validator/sawtooth_validator/journal/batch_injector.py
@@ -14,9 +14,16 @@
 # ------------------------------------------------------------------------------
 
 import abc
+import hashlib
 import importlib
+import uuid
 
 from sawtooth_validator.state.settings_view import SettingsView
+
+from sawtooth_validator.protobuf.batch_pb2 import Batch
+from sawtooth_validator.protobuf.batch_pb2 import BatchHeader
+from sawtooth_validator.protobuf.transaction_pb2 import Transaction
+from sawtooth_validator.protobuf.transaction_pb2 import TransactionHeader
 
 
 class BatchInjectorFactory(metaclass=abc.ABCMeta):
@@ -36,6 +43,14 @@ class BatchInjectorFactory(metaclass=abc.ABCMeta):
 class BatchInjector(metaclass=abc.ABCMeta):
     """The interface to implement for injecting batches during block
     publishing."""
+
+    @property
+    def version():
+        raise NotImplementedError()
+
+    @property
+    def identifier():
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def block_start(self, previous_block):
@@ -60,8 +75,6 @@ class DefaultBatchInjectorFactory:
     def __init__(self, state_view_factory, signer):
         """
         Args:
-            block_cache (:obj:`BlockCache`): The block cache, for passing to
-                batch injectors that require it.
             state_view_factory (:obj:`StateViewFactory`): The state view
                 factory, for passing to injectors that require it.
             signer (:obj:`Signer`): The cryptographic signer of the validator.
@@ -78,8 +91,9 @@ class DefaultBatchInjectorFactory:
             else batch_injector_setting.split(',')
 
     def create_injectors(self, state_root_hash):
-        injectors = self._read_injector_setting(state_root_hash)
-        return [self._create_injector(i) for i in injectors]
+        #injectors = self._read_injector_setting(state_root_hash)
+        # return [self._create_injector(i) for i in injectors]
+        return [GluwaBatchInjector(self._signer)]
 
     def _create_injector(self, injector):
         """Returns a new batch injector"""
@@ -91,3 +105,53 @@ class DefaultBatchInjectorFactory:
             return GluwaBatchInjector(self._signer)
 
         raise UnknownBatchInjectorError(injector)
+
+
+class GluwaBatchInjector(BatchInjector):
+    housekeeping_payload = b'\xa2avlHousekeepingbp1a0'
+
+    def __init__(self, signer):
+        self._signer = signer
+
+    def block_start(self, previous_block):
+        pub_key = self._signer.get_public_key().as_hex()
+        version = '1.7'
+        ns = '8a1a04'
+        payload = GluwaBatchInjector.housekeeping_payload
+        tx_header = TransactionHeader(
+            family_name='CREDITCOIN',
+            family_version=version,
+            inputs=[ns],
+            outputs=[ns],
+            nonce=str(uuid.uuid4()),
+            batcher_public_key=pub_key,
+            dependencies=[],
+            signer_public_key=pub_key,
+            payload_sha512=hashlib.sha512(payload).hexdigest()
+        )
+        tx_header_str = tx_header.SerializeToString()
+        tx_header_signature = self._signer.sign(tx_header_str)
+        tx = Transaction(
+            payload=payload,
+            header=tx_header_str,
+            header_signature=tx_header_signature
+        )
+        txn_signatures = [tx.header_signature]
+        batch_header = BatchHeader(
+            signer_public_key=pub_key,
+            transaction_ids=txn_signatures
+        )
+        batch_header_str = batch_header.SerializeToString()
+        batch_header_signature = self._signer.sign(batch_header_str)
+        batch = Batch(
+            header=batch_header_str,
+            transactions=[tx],
+            header_signature=batch_header_signature
+        )
+        return [batch]
+
+    def version():
+        return "0.1"
+
+    def identifier():
+        return "GluwaHousekeeping"
